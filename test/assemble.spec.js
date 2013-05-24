0b36d8bf4702585c4b920c6f1b736c7abf9e4e0e
Howard Engelhart <howard.engelhart@outlook.com>
2013-05-24 01:47:01 -0400

    added in assemble function

---
+++ test/assemble.spec.js
@@ -0,0 +1,36 @@
+var path = require('path'),
+    fs   = require('fs');
+    mux     = require('../../mux');
+
+describe('assemble test suite',function(){
+    it('should have an assembler object defined',function(){
+        expect(mux.assemble).toBeDefined();
+    });
+
+
+    it('should assemble',function(done){
+        var template = {
+            duration    : 16.5,
+            bitrate     : '48k',
+            frequency   : 22050,
+            output      : path.join(__dirname,'result.mp3'),
+            workspace   : __dirname,
+            playList    : [
+                { ts: 2, src: path.join(__dirname,'b0.mp3')},
+                { ts: 8, src: path.join(__dirname,'b1.mp3')},
+                { ts: 13, src: path.join(__dirname,'b2.mp3')}
+            ]
+        };
+
+        mux.assemble(template,function(err,tmpl){
+            expect(err).toBeNull();
+            expect(tmpl).toBe(template);
+            expect(fs.existsSync(tmpl.output)).toEqual(true);
+            done();
+        });
+
+
+    });
+});
+
+
