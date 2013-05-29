33cf9aa50d12f61ff50c204f8cbb4edc84617f74
Howard Engelhart <howard.engelhart@outlook.com>
2013-05-29 08:07:09 -0400

    scripting working

--- bin/mux.js
+++ bin/mux.js
@@ -1,54 +1,89 @@
 #!/usr/bin/env node
-
-var fs       = require('fs'),
+/*
+ * What it should do....
+ * 1. load a template file containing strings + timestamps + video?
+ * 2. load configuration specifying tts service, working directory? caching?
+ * 3. create job object from template
+ * 4. iterate through each script item
+ *  a. create sha1 based on (lc) text.
+ *  b. assign sha1 to item in job object
+ *  c. search cache for sound file
+ *  d. if not found, create mp3 and cache, name using sha1
+ * 5. If found all items in cache, check cache for composite sha1 (sha1 of all sha1's)
+ *   a. if found use that mp3
+ *   b. if not found create that mp3
+ * 6. Create video name sha1 (concat mp3 sha1 + video src type sha1 + video name sha1)
+ * 7. If found in cache, return that video, else
+ * 8. Generate amerge of video, cache using video sha1
+ * 9. Return link to merged video
+ */
+var fs       = require('fs-extra'),
     path     = require('path'),
-    program  = require('commander'),
-    mux      = (path.join(__dirname,'../../mux')),
-    request, template;
+    crypto   = require('crypto'),
+    mux      = require(path.join(__dirname,'../../mux'));
 
-program
-    .version('0.0.1')
-    .option('-v, --video-type [type]', 'Specify video mime type [video/mp4]','video/mp4')
-    .parse(process.argv);
-
-if (!program.args[0]){
-    exitApp('Expected a template file.',1);
-}
-
-try {
-    template = loadTemplateFromFile(program.args[0]);
-} catch (e) {
-    exitApp('Load template failed with: ' + e.message, 1);
-}
-
-try {
-    if (program.args[1]){
-        request = loadRequestFromFile(program.args[1]);    
+if (!process.env['ut-mux-bin']){
+    try {
+        main(function(rc,msg){
+            exitApp(rc,msg);
+        });
+    } catch(e) {
+        exitApp(1,e.message);
     }
-} catch (e){
-    exitApp('Load request failed with: ' + e.message, 1);
 }
 
-if (!request) {
-    exitApp('Need a request',1);
-}
+function main(done){
+    var program  = require('commander'),
+        config, job;
+    
+    program
+        .version('0.0.1')
+        .option('-c, --config [cfgfile]','Specify config file [undefined]',undefined)
+        .option('-v, --video-type [type]', 'Specify video mime type [video/mp4]','video/mp4')
+        .parse(process.argv);
 
-if (!request.srcType) {
-    request.srcType = program.videoType;
-}
 
-function xxx() {
+    if (!program.args[0]){
+        throw new SyntaxError('Expected a template file.');
+    }
 
-    this.interpolateTemplates = function(data) {
-        var annoLength = this.model.annotations.length;
-        $log.info('Interpolate ' + annoLength + ' annotations with ' + data.length + ' responses.');
-        for (var i = 0; i < annoLength; i++) {
-            var a = this.model.annotations[i];
-            a.text = this.interpolate(a.template,data);
-            $log.info('Annotation [' + i + ']: ' + a.text);
+    config = createConfiguration(program.config);
+    config.ensurePaths();
+
+    job = createMuxJob(loadTemplateFromFile(program.args[0]),config);
+
+    //console.log('JOB: ' + JSON.stringify(job,null,3));
+
+    var pipeline = [];
+    if (!job.hasOutput()){
+        pipeline.unshift(applyScriptToVideo);
+        if (!job.hasScript()){
+            pipeline.unshift(convertScriptToMP3);
+            if (!job.hasVideoLength()){
+                pipeline.unshift(getVideoLength);
+            }
+
+            if (!job.hasLines()){
+                pipeline.unshift(convertLinesToMP3);
+            }
         }
-    };
+        
+        if (!job.hasVideo()){
+            pipeline.unshift(getSourceVideo);
+        }
+    } 
 
+    if (pipeline.length !== 0){
+        pipelineJob(job,pipeline,function(err,job,lastFn){
+            if (err) {
+                done(1,'Died on [' + lastFn.name + ']:' + err.message);
+            } else {
+                done(0,'Done with work');
+            }
+        });
+    }  else {
+        done(0,'Done from cache');
+    }
 }
 
 /*
@@ -56,7 +91,7 @@ function xxx() {
  *
  */
 
-function exitApp (msg,resultCode){
+function exitApp (resultCode,msg){
     if (msg){
         if (resultCode){
             console.error(msg);
@@ -69,53 +104,234 @@ function exitApp (msg,resultCode){
 
 
 function loadTemplateFromFile(tmplFile){
-    var tmplObj = JSON.parse(fs.readFileSync(tmplFile));
-
-    if (!(tmplObj.annotations instanceof Object)){
-        throw new SyntaxError('Template is missing annotations section');
+    var buff,tmplobj;
+ 
+    buff = fs.readFileSync(tmplFile);
+    try {
+        tmplObj = JSON.parse(buff);
+    } catch(e) {
+        throw new Error('Error parsing template: ' + e.message);
     }
 
-    if ((!(tmplObj.annotations.notes instanceof Array)) || (!tmplObj.annotations.notes.length)){
-        throw new SyntaxError('Template is missing notes section');
+    if ((!(tmplObj.script instanceof Array)) || (!tmplObj.script.length)){
+        throw new SyntaxError('Template is missing script section');
     }
 
     return tmplObj;
 }
 
-function loadRequestFromFile(rqsFile){
-    var rqsObj = JSON.parse(fs.readFileSync(rqsFile));
+function createConfiguration(cfgFile){
+    var userCfg,cfgObject = {
+        caches : {
+                    line    : path.normalize('/usr/local/share/mux/caches/line/'),
+                    script  : path.normalize('/usr/local/share/mux/caches/script/'),
+                    video   : path.normalize('/usr/local/share/mux/caches/video/'),
+                    output  : path.normalize('/usr/local/share/mux/caches/output/')
+                 },
+        ttsAuth : path.join(process.env['HOME'],'.tts.json'),
+        bitrate     : '48k',
+        frequency   : 22050,
+        workspace   : __dirname
+    };
 
-    if ((!(rqsObj.responses instanceof Array)) || (!rqsObj.responses.length)){
-        throw new SyntaxError('Request is missing responses section');
+    if (cfgFile) { 
+        userCfg = JSON.parse(fs.readFileSync(cfgFile));
     }
-    
-    return rqsObj;
+ 
+    if (userCfg) {
+        Object.keys(userCfg).forEach(function(key){
+            cfgObject[key] = userCfg[key];
+        });
+    }
+
+    cfgObject.ensurePaths = function(){
+        var self = this;
+        Object.keys(self.caches).forEach(function(key){
+            if (!fs.existsSync(self.caches[key])){
+                fs.mkdirsSync(self.caches[key]);
+            }
+        });
+    };
+
+    cfgObject.cacheAddress = function(fname,cache){
+        return path.join(this.caches[cache],fname); 
+    }
+
+    return cfgObject;
 }
 
-function interpolate (tmpl,data) {
-    var patt  = /\${(\d+)}/,
-        dataLen,
-        match;
+function createMuxJob(template,config){
+    var buff,
+        obj       = {},
+        soh       = String.fromCharCode(1),
+        videoExt  = path.extname(template.video),
+        videoBase = path.basename(template.video,videoExt);
 
-    if (!data) {
-        return tmpl;
+    obj.tracks = [];
+    template.script.forEach(function(item){
+        var track = {
+            ts      : Number(item.ts),
+            line    : item.line,
+            hash    : hashText(item.line.toLowerCase())
+        };
+        track.fname   = (track.hash + '.mp3'),
+        track.fpath   = config.cacheAddress(track.fname,'line')
+        obj.tracks.push(track);
+        buff = (soh + track.ts + ':' + track.hash);
+    });
+
+    obj.scriptHash = hashText(buff);
+    obj.scriptPath = config.cacheAddress(videoBase + '_' + obj.scriptHash + '.mp3','script');
+    obj.videoPath  = config.cacheAddress(template.video,'video');
+  
+    obj.outputHash = hashText(template.video + ':' + obj.scriptHash);
+    obj.outputPath = config.cacheAddress((videoBase + '_' + obj.outputHash + videoExt),'output');
+   
+    obj.hasVideoLength = function(){
+        return (this.videoLength && (this.videoLength > 0));
+    };
+
+    obj.hasVideo = function(){
+        return fs.existsSync(this.videoPath);
+    };
+
+    obj.hasOutput = function(){
+        return fs.existsSync(this.outputPath);
+    };
+
+    obj.hasScript = function(){
+        return fs.existsSync(this.scriptPath);
     }
 
-    if ((data instanceof Array) === false) {
-        throw new TypeError('Data parameter must be an array.'); 
-    }
-
-    dataLen = data.length;
-    while((match = patt.exec(tmpl)) !== null) {
-        var idx = (match[1] - 1);
-        if (idx < 0) {
-            throw new RangeError('Template parameters should start at ${1}');
+    obj.hasLines = function(){
+        for (var i =0; i < this.tracks.length; i++){
+            if (!fs.existsSync(this.tracks[i].fpath)){
+                return false;
+            }
         }
-        if (idx >= dataLen) {
-            throw new RangeError('Invalid template parameter (too high): ' + match[0]);
-        }
-        tmpl = tmpl.replace(match[0],data[idx]);
+        return true;
     }
-    return tmpl;
+
+    obj.ttsAuth = mux.vocalWare.createAuthToken(config.ttsAuth);
+
+    obj.assembleTemplate = function(){
+        var self = this;
+        result = {
+            duration  : self.videoLength,
+            bitrate   : config.bitrate,
+            frequency : config.frequency,
+            workspace : config.workspace,
+            output    : self.scriptPath
+        };
+        result.playList = [];
+        self.tracks.forEach(function(track){
+            result.playList.push({
+                ts  : track.ts,
+                src : track.fpath
+            });
+        });
+
+        return result;
+    }
+
+    return obj;
+}
+
+function hashText(txt){
+    var hash = crypto.createHash('sha1');
+    hash.update(txt);
+    return hash.digest('hex');
+}
+
+function pipelineJob(job,pipeline,handler){
+    var fn = pipeline.shift();
+    if (fn) {
+        console.log('Run: ' + fn.name);
+        fn(job,function(err){
+            if (err) {
+                handler(err,job,fn);
+            } else {
+                process.nextTick(function(){
+                    pipelineJob(job,pipeline,handler);
+                });
+            }
+        });
+    } else {
+        handler(null,job,null);
+    }
+}
+
+        
+function applyScriptToVideo(job,done){
+    done();
+}
+
+function convertScriptToMP3(job,done){
+    mux.assemble(job.assembleTemplate(),function(err,tmpl){
+        if (err) {
+            done(err);
+            return;
+        }
+        console.log('Assembled: ' + tmpl.output);
+        done();
+    });
+}
+
+function getVideoLength(job,done){
+    mux.ffmpeg.probe(job.videoPath,function(err,info){
+        if (err){
+            done(err);
+            return;
+        }
+
+        if (!info.duration){
+            done({message : 'Unable to determine video length.'});
+            return;
+        }
+
+        job.videoLength = info.duration;
+        console.log('Video length: ' + job.videoLength);
+        done();
+    });
+}
+
+function getSourceVideo(job,done){
+    done({message : 'Need to get the video: ' + job.videoPath});
+}
+
+function convertLinesToMP3(job,done){
+    var rqsCount = 0, errs;
+
+    job.tracks.forEach(function(track){
+        if (!fs.existsSync(track.fpath)){
+            rqsCount++;
+            var rqs = mux.vocalWare.createRequest({authToken : job.ttsAuth});
+            rqs.say(track.line);
+            console.log('Create track: ' + track.fpath);
+            mux.vocalWare.textToSpeech(rqs,track.fpath,function(e,rqs,o){
+                if (e) {
+                    console.error(e.message);
+                    if (!errs) {
+                        errs = [ e ];
+                    } else {
+                        errs.push(e);
+                    }
+                }
+                if (--rqsCount < 1){
+                    if (errs){
+                       done(errs[0]); 
+                    } else {
+                       done();
+                    }
+                }
+            });
+        }
+    });
+
+}
+
+module.exports = {
+    'createConfiguration' : createConfiguration,
+    'createMuxJob'        : createMuxJob
 };
 
---
+++ test/mux.spec.js
@@ -0,0 +1,74 @@
+if (process.env['ut-mux-bin']) {
+
+var path      = require('path'),
+    fs        = require('fs-extra'),
+    mux       = require('../bin/mux');
+
+describe('mux helpers',function(){
+    var rmList = [];
+    afterEach(function(){
+        rmList.forEach(function(removable){
+            if (fs.existsSync(removable)){
+                fs.removeSync(removable);
+            }
+        });
+    });
+
+    it('should create a configuration object without a config file',function(){
+        var cfg = mux.createConfiguration();
+        
+        expect(cfg.caches).toBeDefined();
+        
+        expect(cfg.caches.line).toEqual(path.normalize('/usr/local/share/mux/caches/line/'));
+        expect(cfg.caches.script).toEqual(path.normalize('/usr/local/share/mux/caches/script/'));
+        expect(cfg.caches.video).toEqual(path.normalize('/usr/local/share/mux/caches/video/'));
+        expect(cfg.caches.output).toEqual(path.normalize('/usr/local/share/mux/caches/output/'));
+        
+    });
+
+    it('should throw an error if given a non existant configuration file', function(){
+        expect(function(){
+            mux.createConfiguration('abc.cfg');
+        }).toThrow('ENOENT, no such file or directory \'abc.cfg\'');
+    });
+
+    it('should throw an error if given a badly formed configuration file', function(){
+        rmList.push(path.join(__dirname,'tmpcfg.json'));
+        fs.writeFileSync(path.join(__dirname,'tmpcfg.json'),'abc');
+        expect(function(){
+            mux.createConfiguration(path.join(__dirname,'tmpcfg.json'));
+        }).toThrow('Unexpected token a');
+    });
+
+    it('creates any required dirs with ensurePaths',function(){
+        rmList.push(path.join(__dirname,'caches')); 
+        rmList.push(path.join(__dirname,'tmpcfg.json'));
+
+        fs.writeFileSync(path.join(__dirname,'tmpcfg.json'),JSON.stringify({
+            caches : {
+                        line    : path.join(__dirname,'caches/line/'),
+                        script  : path.join(__dirname,'caches/script/'),
+                        video   : path.join(__dirname,'caches/video/'),
+                        output  : path.join(__dirname,'caches/output/')
+                     }
+        }));
+        var cfg = mux.createConfiguration(path.join(__dirname,'tmpcfg.json'));
+       
+        cfg.ensurePaths();
+        expect(cfg.caches).toBeDefined();
+        
+        expect(cfg.caches.line).toEqual(path.join(__dirname,   'caches/line/'));
+        expect(cfg.caches.script).toEqual(path.join(__dirname, 'caches/script/'));
+        expect(cfg.caches.video).toEqual(path.join(__dirname,  'caches/video/'));
+        expect(cfg.caches.output).toEqual(path.join(__dirname, 'caches/output/'));
+        
+        expect(fs.existsSync(cfg.caches.line)).toBeTruthy();
+        expect(fs.existsSync(cfg.caches.script)).toBeTruthy();
+        expect(fs.existsSync(cfg.caches.video)).toBeTruthy();
+        expect(fs.existsSync(cfg.caches.output)).toBeTruthy();
+    });
+
+});
+
+} // -- END if (process.env['ut-mux-bin']) {
+
