250d42a8d02d04adcbb39d71b772ff0c549324f3
Evan Leichter <evan.leichter@gmail.com>
2013-12-04 11:10:53 -0500

    Updated unit tests for share

--- bin/share.js
+++ bin/share.js
@@ -15,20 +15,8 @@ var include     = require('../lib/inject').require,
     uuid        = include('../lib/uuid'),
     daemon      = include('../lib/daemon'),
     app         = express(),
-
-    // This is the template for share's configuration
-    defaultConfiguration = {
-        caches : {
-            run     : path.normalize('/usr/local/share/cwrx/dub/caches/run/'),
-        },
-        s3 : {
-            share     : {
-                bucket  : 'c6media',
-                path    : 'usr/screenjack/video/'
-            },
-            auth    : path.join(process.env.HOME,'.aws.json')
-        }
-    },
+    
+    share = {}, // for exporting functions to unit tests
 
     // Attempt a graceful exit
     exitApp  = function(resultCode,msg){
@@ -42,6 +30,174 @@ var include     = require('../lib/inject').require,
         }
         process.exit(resultCode);
     };
+
+// This is the template for share's configuration
+share.defaultConfiguration = {
+    caches : {
+        run     : path.normalize('/usr/local/share/cwrx/dub/caches/run/'),
+    },
+    s3 : {
+        share     : {
+            bucket  : 'c6media',
+            path    : 'usr/screenjack/video/'
+        },
+        auth    : path.join(process.env.HOME,'.aws.json')
+    },
+    awesm: {
+        key: 'c6dc7ece16b2950bf2a746a2e2eeabcdefa26be40fb1fdd78642b777d0399759',
+        releaseTool: 'NQlq1r',
+        stagingTool: 'gRgPad'
+    }
+},
+
+share.getVersion = function() {
+    var fpath = path.join(__dirname, 'share.version'),
+        log = logger.getLog();
+        
+    if (fs.existsSync(fpath)) {
+        try {
+            return fs.readFileSync(fpath).toString();
+        } catch(e) {
+            log.error('Error reading version file: ' + e.message);
+        }
+    }
+    log.warn('No version file found');
+    return 'unknown';
+}
+
+share.createConfiguration = function(cmdLine) {
+    var cfgObject = cwrxConfig.createConfigObject(cmdLine.config, share.defaultConfiguration),
+        log;
+
+    if (cfgObject.log) {
+        log = logger.createLog(cfgObject.log);
+    }
+
+    try {
+        aws.config.loadFromPath(cfgObject.s3.auth);
+    }  catch (e) {
+        throw new SyntaxError('Failed to load s3 config: ' + e.message);
+    }
+
+    cfgObject.ensurePaths = function(){
+        var self = this;
+        Object.keys(self.caches).forEach(function(key){
+            log.trace('Ensure cache[' + key + ']: ' + self.caches[key]);
+            if (!fs.existsSync(self.caches[key])){
+                log.trace('Create cache[' + key + ']: ' + self.caches[key]);
+                fs.mkdirsSync(self.caches[key]);
+            }
+        });
+    };
+
+    cfgObject.cacheAddress = function(fname,cache){
+        return path.join(this.caches[cache],fname);
+    };
+    
+    return cfgObject;
+}
+
+share.shortenUrl = function(origUrl, config, params) {
+    var deferred = q.defer(),
+        log = logger.getLog(),
+        options = {
+            url: 'http://api.awe.sm/url.json?v=3&key=' + config.awesm.key + '&channel=email'
+        };
+        
+    if (params && params.tag === 'release') {
+        log.trace('using release sharer');
+        options.url += '&tool=' + config.awesm.releaseTool;
+        options.url += '&tag=release';
+    } else {
+        log.trace('using staging sharer');
+        options.url += '&tool=' + config.awesm.stagingTool;
+        options.url += '&tag=staging';
+    }
+    for (var key in (params || {})) {
+        if (key === 'tag') continue;
+        options.url += '&' + key + '=' + encodeURIComponent(params[key]);
+    };
+    options.url += '&url=' + encodeURIComponent(origUrl);
+    log.trace(options.url);
+    request.post(options, function(error, response, body) {
+        var data;
+        try {
+            data = JSON.parse(body);
+        } catch(e) {
+            deferred.reject('error parsing response as json');
+            return;
+        }
+        if (error || data.error) {
+            deferred.reject(error || body);
+            return;
+        }
+        deferred.resolve(data.awesm_url);
+    });
+    return deferred.promise;
+}
+
+share.shareLink = function(req, config, done) {
+    var log = logger.getLog(),
+        body = req.body;
+    log.info("[%1] Starting shareLink", req.uuid);
+
+    if (!body || !body.origin) {
+        log.error("[%1] No origin url in request", req.uuid);
+        done("You must include the origin url to generate a shareable url");
+        return;
+    }
+    var origin = body.origin,
+        item = body.data,
+        prefix = body.origin.split('experiences/')[0];
+
+    var generateUrl = function(uri) {
+        var url;
+        if (!uri) {
+            url = body.origin;
+        } else {
+            url = prefix + 'experiences/';
+            url += uri;
+        }
+        
+        share.shortenUrl(url, config, req.body.awesm_params).then(function(shortUrl) {
+            log.info("[%1] Finished shareLink: URL = %2, short = %3", req.uuid, url, shortUrl);
+            done(null, url, shortUrl);
+        }).catch(function(error) {
+            log.error('[%1] Failed to shorten url: url = %2, error = %3', req.uuid, url, error);
+            done(null, url, null);
+        });
+    };
+
+    if (!item) {
+        generateUrl();
+        return;
+    }
+
+    var s3 = new aws.S3(),
+        deferred = q.defer(),
+        id = 'e-' + uuid.createUuid().substr(0,14),
+        fname = id + '.json',
+        params = { Bucket       : config.s3.share.bucket,
+                   ACL          : 'public-read',
+                   ContentType  : 'application/JSON',
+                   Key          : path.join(config.s3.share.path, fname)
+                 };
+    
+    item.id = id;
+    item.uri = item.uri.replace('shared~', '');
+    item.uri = 'shared~' + item.uri.split('~')[0] + '~' + id;
+    params.Body = (item ? new Buffer(JSON.stringify(item)) : null);
+
+    log.info("[%1] Uploading data: Bucket = %2, Key = %3", req.uuid, params.Bucket, params.Key);
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            done(err);
+        } else {
+            log.trace('[%1] SUCCESS: ' + JSON.stringify(data), req.uuid);
+            generateUrl(item.uri);
+        }
+    });
+}
 
 if (!__ut__){
     try {
@@ -80,7 +236,7 @@ function main(done) {
 
     program.enableAws = true;
 
-    config = createConfiguration(program);
+    config = share.createConfiguration(program);
 
     if (program.showConfig){
         console.log(JSON.stringify(config,null,3));
@@ -117,7 +273,7 @@ function main(done) {
         return done(0,'Exit');
     });
 
-    log.info('Running version ' + getVersion());
+    log.info('Running version ' + share.getVersion());
     
     // Daemonize if so desired
     if ((program.daemon) && (process.env.RUNNING_AS_DAEMON === undefined)) {
@@ -146,7 +302,7 @@ function main(done) {
     });
 
     app.post('/share', function(req, res, next) {
-        shareLink(req, config, function(err, output, short_url) {
+        share.shareLink(req, config, function(err, output, shortUrl) {
             if (err) {
                 res.send(400,{
                     error  : 'Unable to complete request.',
@@ -156,7 +312,7 @@ function main(done) {
             }
             res.send(200, {
                 url: output,
-                short_url: short_url
+                shortUrl: shortUrl
             });
         });
     });
@@ -165,160 +321,6 @@ function main(done) {
     log.info('Share server is listening on port: ' + program.port);
 }
 
-function getVersion() {
-    var fpath = path.join(__dirname, 'share.version'),
-        log = logger.getLog();
-        
-    if (fs.existsSync(fpath)) {
-        try {
-            return fs.readFileSync(fpath).toString();
-        } catch(e) {
-            log.error('Error reading version file: ' + e.message);
-        }
-    }
-    log.warn('No version file found');
-    return 'unknown';
-}
-
-function createConfiguration(cmdLine) {
-    var cfgObject = cwrxConfig.createConfigObject(cmdLine.config, defaultConfiguration),
-        log;
-
-    if (cfgObject.log) {
-        log = logger.createLog(cfgObject.log);
-    }
-
-    try {
-        aws.config.loadFromPath(cfgObject.s3.auth);
-    }  catch (e) {
-        throw new SyntaxError('Failed to load s3 config: ' + e.message);
-    }
-
-    cfgObject.ensurePaths = function(){
-        var self = this;
-        Object.keys(self.caches).forEach(function(key){
-            log.trace('Ensure cache[' + key + ']: ' + self.caches[key]);
-            if (!fs.existsSync(self.caches[key])){
-                log.trace('Create cache[' + key + ']: ' + self.caches[key]);
-                fs.mkdirsSync(self.caches[key]);
-            }
-        });
-    };
-
-    cfgObject.cacheAddress = function(fname,cache){
-        return path.join(this.caches[cache],fname);
-    };
-    
-    return cfgObject;
-}
-
-function shareLink(req, config, done) {
-    var log = logger.getLog(),
-        body = req.body;
-    log.info("[%1] Starting shareLink", req.uuid);
-
-    if (!body || !body.origin) {
-        log.error("[%1] No origin url in request", req.uuid);
-        done("You must include the origin url to generate a shareable url");
-        return;
-    }
-    var origin = body.origin,
-        item = body.data,
-        prefix = body.origin.split('experiences/')[0];
-
-    var generateUrl = function(uri) {
-        var url;
-        if (!uri) {
-            url = body.origin;
-        } else {
-            url = prefix + 'experiences/';
-            url += uri;
-        }
-        
-        shortenUrl(url, req.body.awesm_params).then(function(short_url) {
-            log.info("[%1] Finished shareLink: URL = %2, short = %3", req.uuid, url, short_url);
-            done(null, url, short_url);
-        }).catch(function(error) {
-            log.error('[%1] Failed to shorten url: url = %2, error = %3', req.uuid, url, error);
-            done(null, url, null);
-        });
-    };
-
-    if (!item) {
-        generateUrl();
-        return;
-    }
-
-    var s3 = new aws.S3(),
-        deferred = q.defer(),
-        id = 'e-' + uuid.createUuid().substr(0,14),
-        fname = id + '.json',
-        params = { Bucket       : config.s3.share.bucket,
-                   ACL          : 'public-read',
-                   ContentType  : 'application/JSON',
-                   Key          : path.join(config.s3.share.path, fname)
-                 };
-    
-    item.id = id;
-    item.uri = item.uri.replace('shared~', '');
-    item.uri = 'shared~' + item.uri.split('~')[0] + '~' + id;
-    params.Body = (item ? new Buffer(JSON.stringify(item)) : null);
-
-    log.info("[%1] Uploading data: Bucket = %2, Key = %3", req.uuid, params.Bucket, params.Key);
-    s3.putObject(params, function(err, data) {
-        if (err) {
-            done(err);
-        } else {
-            log.trace('[%1] SUCCESS: ' + JSON.stringify(data), req.uuid);
-            generateUrl(item.uri);
-        }
-    });
-}
-
-function shortenUrl(orig_url, params) {
-    var deferred = q.defer(),
-        log = logger.getLog(),
-        options = {
-            url: 'http://api.awe.sm/url.json?v=3&key=c6dc7ece16b2950bf2a746a2e2eeabcdefa26be40fb1fdd78642b777d0399759&channel=email'
-        };
-        
-    if (params && params.tag === 'release') {
-        log.trace('using release sharer');
-        options.url += '&tool=NQlq1r';
-        options.url += '&tag=release';
-    } else {
-        log.trace('using staging sharer');
-        options.url += '&tool=gRgPad';
-        options.url += '&tag=staging';
-    }
-    for (var key in (params || {})) {
-        if (key === 'tag') continue;
-        options.url += '&' + key + '=' + encodeURIComponent(params[key]);
-    };
-    options.url += '&url=' + encodeURIComponent(orig_url);
-    log.trace(options.url);
-    request.post(options, function(error, response, body) {
-        var data;
-        try {
-            data = JSON.parse(body);
-        } catch(e) {
-            deferred.reject('error parsing response as json');
-            return;
-        }
-        if (error || data.error) {
-            deferred.reject(error || body);
-            return;
-        }
-        deferred.resolve(data.awesm_url);
-    });
-    return deferred.promise;
-}
-
 if (__ut__) {
-    module.exports = {
-        getVersion: getVersion,
-        createConfiguration: createConfiguration,
-        defaultConfiguration: defaultConfiguration,
-        shareLink: shareLink
-    };
+    module.exports = share;
 }
--- test/unit/share.svc.spec.js
+++ test/unit/share.svc.spec.js
@@ -1,5 +1,8 @@
 var path        = require('path'),
     fs          = require('fs-extra'),
+    q           = require('q'),
+    request     = require('request'),
+    querystring = require('querystring'),
     cwrxConfig  = require('../../lib/config'),
     uuid        = require('../../lib/uuid'),
     sanitize    = require('../sanitize');
@@ -147,27 +150,139 @@ describe('share (UT)', function() {
             expect(cfgObject.cacheAddress('test.pid', 'run')).toBe('ut/run/test.pid');
         });
     });
+    
+    describe('shortenUrl', function() {
+        var config, url;
+        
+        beforeEach(function() {
+            url = 'http://cinema6.com';
+            config = {
+                awesm: {
+                    key: 'awesmKey',
+                    releaseTool: 'relTool',
+                    stagingTool: 'stagTool'
+                }
+            };
+            spyOn(request, 'post');
+        });
+        
+        it('should exist', function() {
+            expect(share.shortenUrl).toBeDefined();
+        });
+        
+        it('should properly send a request to awesm', function(done) {
+            request.post.andCallFake(function(opts, cb) {
+                cb(null, null, '{"awesm_url": "http://cinema6.com/short"}');
+            });
+            
+            share.shortenUrl(url, config, null).then(function(shortUrl) {
+                expect(shortUrl).toBe('http://cinema6.com/short');
+                expect(request.post).toHaveBeenCalled();
+                var opts = request.post.calls[0].args[0];
+                expect(opts.url.match(/^http:\/\/api.awe.sm\/url\.json\?v=3/)).toBeTruthy();
+                
+                var query = querystring.parse(opts.url.split('?')[1]);
+                expect(query.key).toBe('awesmKey');
+                expect(query.tag).toBe('staging');
+                expect(query.tool).toBe('stagTool');
+                expect(query.url).toBe('http://cinema6.com');
+                done();
+            }).catch(function(error) {
+                expect(error).not.toBeDefined();
+                done();
+            });
+        });
+        
+        it('should handle errors properly', function(done) {
+            request.post.andCallFake(function(opts, cb) {
+                cb('Error!', null, null);
+            });
+            share.shortenUrl(url, config, null).catch(function(error) {
+                expect(error).toBe('Error!');
+            });
+            
+            request.post.andCallFake(function(opts, cb) {
+                cb(null, null, '{"error": "error in body"}');
+            });
+            share.shortenUrl(url, config, null).catch(function(error) {
+                expect(error).toBe('{"error": "error in body"}');
+            });
+            
+            request.post.andCallFake(function(opts, cb) {
+                cb(null, null, 'improper json');
+            });
+            share.shortenUrl(url, config, null).catch(function(error) {
+                expect(error).toBe('error parsing response as json');
+                done();
+            });
+        });
+        
+        it('should properly use awesm params passed to it', function(done) {
+            var params = {
+                tag: 'release',
+                campaign: 'utCampaign',
+                notes: 'utNotes'
+            }
+            request.post.andCallFake(function(opts, cb) {
+                cb(null, null, '{"awesm_url": "http://cinema6.com/short"}');
+            });
+            
+            share.shortenUrl(url, config, params).then(function(shortUrl) {
+                expect(shortUrl).toBe('http://cinema6.com/short');
+                expect(request.post).toHaveBeenCalled();
+                
+                var opts = request.post.calls[0].args[0];
+                var query = querystring.parse(opts.url.split('?')[1]);
+                expect(query.tool).toBe('relTool');
+                expect(query.tag).toBe('release');
+                expect(query.campaign).toBe('utCampaign');
+                expect(query.notes).toBe('utNotes');
+                done();
+            }).catch(function(error) {
+                expect(error).not.toBeDefined();
+                done();
+            });
+        });
+    });
 
     describe('shareLink', function() {
+        var config, req, uuidSpy;
+                
+        beforeEach(function() {
+            spyOn(share, 'shortenUrl').andCallFake(function(orig) {
+                return q(orig + '?short=true');
+            });
+            uuidSpy = spyOn(uuid, 'createUuid').andReturn('ut1');
+            config = {
+                s3: {
+                    share: {
+                        bucket: 'ut_bucket',
+                        path: 'ut/'
+                    }
+                }
+            };
+            req = {
+                uuid: 'abc123',
+                body: {
+                    origin: 'http://cinema6.com/#/experiences/utApp~oldId',
+                }
+            };
+        });
+    
         it('should exist', function() {
             expect(share.getVersion).toBeDefined();
         });
         
-        it('should correctly return a link if not given an experience object', function() {
-            var req = {
-                uuid: 'abc123',
-                body: {
-                    origin: 'http://cinema6.com/#/experiences/ut'
-                }
-            };
-            
-            share.shareLink(req, null, function (err, url) {
+        it('should correctly return a link if not given an experience object', function(done) {
+            share.shareLink(req, null, function (err, url, shortUrl) {
                 expect(err).toBeNull();
-                expect(url).toBe('http://cinema6.com/#/experiences/ut');
+                expect(url).toBe('http://cinema6.com/#/experiences/utApp~oldId');
+                expect(shortUrl).toBe('http://cinema6.com/#/experiences/utApp~oldId?short=true');
+                done();
             });
         });
 
-        it('should fail if not given an origin url', function() {
+        it('should fail if not given an origin url', function(done) {
             req = { uuid: 'abc123' };
             
             share.shareLink(req, null, function(err, url) {
@@ -178,73 +293,76 @@ describe('share (UT)', function() {
             req.body = {data: {id: 'e-1'}};
             share.shareLink(req, null, function(err, url) {
                 expect(err).toBeDefined('err');
-                expect(url).not.toBeDefined();                
+                expect(url).not.toBeDefined();
+                expect(errorSpy.calls.length).toBe(2);
+                expect(share.shortenUrl).not.toHaveBeenCalled();
+                done();
             });
-            
-            expect(errorSpy.calls.length).toBe(2);
         });
-
-        describe('tests with S3', function() {
-            var config, req, uuidSpy;
-                
-            beforeEach(function() {
-                uuidSpy = spyOn(uuid, 'createUuid').andReturn('ut1');
-                config = {
-                    s3: {
-                        share: {
-                            bucket: 'ut_bucket',
-                            path: 'ut/'
-                        }
-                    }
-                };
-                req = {
-                    uuid: 'abc123',
-                    body: {
-                        origin: 'http://cinema6.com/#/experiences/utApp~oldId',
-                        data: {
-                            id: 'oldId',
-                            uri: 'utApp~oldId',
-                            title: 'Test Experience'
-                        }
-                    }
-                };
-            });
-                    
-            it('should correctly upload an experience and return a link for it', function() {
-                putObjSpy.andCallFake(function(params, cb) {
-                    cb(null, 'Success!');
-                });
-                share.shareLink(req, config, function(err, url) {
-                    expect(err).toBeNull();
-                    expect(url).toBe('http://cinema6.com/#/experiences/shared~utApp~e-ut1');
-                    
-                    expect(putObjSpy).toHaveBeenCalled();
-                    var putParams = putObjSpy.calls[0].args[0];
-                    expect(putParams.Bucket).toBe('ut_bucket');
-                    expect(putParams.Key).toBe('ut/e-ut1.json');
-                    expect(putParams.ACL).toBe('public-read');
-                    expect(putParams.ContentType).toBe('application/JSON');
-                    
-                    var modExp = JSON.parse(putParams.Body);
-                    expect(modExp).toBeDefined();
-                    expect(modExp.id).toBe('e-ut1');
-                    expect(modExp.uri).toBe('shared~utApp~e-ut1');
-                    expect(modExp.title).toBe('Test Experience');
-                });
+        
+        it('should handle failures to shorten the url', function(done) {
+            share.shortenUrl.andCallFake(function() {
+                return q.reject('Error!');
             });
             
-            it('should handle S3 failures', function() {
-                putObjSpy.andCallFake(function(params, cb) {
-                    cb('Oh noes S3 broke', null);
-                });
-                
-                share.shareLink(req, config, function(err, url) {
-                    expect(putObjSpy).toHaveBeenCalled();
-                    expect(err).toBe('Oh noes S3 broke');
-                    expect(url).not.toBeDefined();
-                });
+            share.shareLink(req, null, function(err, url, shortUrl) {
+                expect(err).toBeNull();
+                expect(url).toBe('http://cinema6.com/#/experiences/utApp~oldId');
+                expect(shortUrl).toBeNull();
+                expect(errorSpy).toHaveBeenCalled();
+                done();
             });
-        }); // end -- describe tests with S3
+        });
+                    
+        it('should correctly upload an experience and return a link for it', function(done) {
+            req.body.data = {
+                id: 'oldId',
+                uri: 'utApp~oldId',
+                title: 'Test Experience'
+            };
+            putObjSpy.andCallFake(function(params, cb) {
+                cb(null, 'Success!');
+            });
+            share.shareLink(req, config, function(err, url, shortUrl) {
+                expect(err).toBeNull();
+                expect(url).toBe('http://cinema6.com/#/experiences/shared~utApp~e-ut1');
+                expect(shortUrl).toBe('http://cinema6.com/#/experiences/shared~utApp~e-ut1?short=true');
+                
+                expect(putObjSpy).toHaveBeenCalled();
+                var putParams = putObjSpy.calls[0].args[0];
+                expect(putParams.Bucket).toBe('ut_bucket');
+                expect(putParams.Key).toBe('ut/e-ut1.json');
+                expect(putParams.ACL).toBe('public-read');
+                expect(putParams.ContentType).toBe('application/JSON');
+                
+                var modExp = JSON.parse(putParams.Body);
+                expect(modExp).toBeDefined();
+                expect(modExp.id).toBe('e-ut1');
+                expect(modExp.uri).toBe('shared~utApp~e-ut1');
+                expect(modExp.title).toBe('Test Experience');
+                done();
+            });
+        });
+        
+        it('should handle S3 failures', function(done) {
+            req.body.data = {
+                id: 'oldId',
+                uri: 'utApp~oldId',
+                title: 'Test Experience'
+            };
+            putObjSpy.andCallFake(function(params, cb) {
+                cb('Oh noes S3 broke', null);
+            });
+            
+            share.shareLink(req, config, function(err, url, shortUrl) {
+                expect(putObjSpy).toHaveBeenCalled();
+                expect(err).toBe('Oh noes S3 broke');
+                expect(url).not.toBeDefined();
+                expect(shortUrl).not.toBeDefined();
+                expect(share.shortenUrl).not.toHaveBeenCalled();
+                done();
+            });
+        });
     }); // end -- describe shareLink
 }); // end -- describe share
 
