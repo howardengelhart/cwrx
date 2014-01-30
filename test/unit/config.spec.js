c368835580f64dad256b246c9d60eb1001f25292
howard <howard.engelhart@outlook.com>
2014-01-30 17:28:10 -0500

    made config work better

--- test/unit/config.spec.js
+++ test/unit/config.spec.js
@@ -1,31 +1,29 @@
-var path      = require('path'),
-    fs        = require('fs-extra'),
-    config    = require('../../lib/config');
 
 describe('config', function() {
-    var rmList = [];
-    afterEach(function(){
-        rmList.forEach(function(removable){
-            if (fs.existsSync(removable)){
-                fs.removeSync(removable);
-            }
-        });
+    var path, fs, config, flush;
+    beforeEach(function(){
+        if (flush){ for (var m in require.cache){ delete require.cache[m]; } flush = false; }
+
+        path   = require('path');
+        fs     = require('fs-extra');
+        config = require('../../lib/config');
+
+        spyOn(fs,'readJsonSync');
     });
-    
+
     describe('createConfiguration', function() {
-        var defaultCfg = {
-            caches : {
-                run     : path.normalize('/usr/local/share/cwrx/dub/caches/run/'),
-                line    : path.normalize('/usr/local/share/cwrx/dub/caches/line/'),
-                blanks  : path.normalize('/usr/local/share/cwrx/dub/caches/blanks/'),
-                script  : path.normalize('/usr/local/share/cwrx/dub/caches/script/'),
-                video   : path.normalize('/usr/local/share/cwrx/dub/caches/video/'),
-                output  : path.normalize('/usr/local/share/cwrx/dub/caches/output/')
-            }
-        };
-        
         it('should create a configuration object without a config file',function(){
-            var cfg = config.createConfigObject('', defaultCfg);
+            var defaultCfg = {
+                caches : {
+                    run     : path.normalize('/usr/local/share/cwrx/dub/caches/run/'),
+                    line    : path.normalize('/usr/local/share/cwrx/dub/caches/line/'),
+                    blanks  : path.normalize('/usr/local/share/cwrx/dub/caches/blanks/'),
+                    script  : path.normalize('/usr/local/share/cwrx/dub/caches/script/'),
+                    video   : path.normalize('/usr/local/share/cwrx/dub/caches/video/'),
+                    output  : path.normalize('/usr/local/share/cwrx/dub/caches/output/')
+                }
+            },
+            cfg = config.createConfigObject('', defaultCfg);
             
             expect(cfg.caches).toBeDefined();
             expect(cfg.caches.run).toEqual(path.normalize('/usr/local/share/cwrx/dub/caches/run/'));
@@ -35,19 +33,39 @@ describe('config', function() {
             expect(cfg.caches.video).toEqual(path.normalize('/usr/local/share/cwrx/dub/caches/video/'));
             expect(cfg.caches.output).toEqual(path.normalize('/usr/local/share/cwrx/dub/caches/output/'));
         });
+           
+        it('should merge config file data with default config',function(){
+            var defaultCfg = {
+                'settingA' : 'banana',
+                'settingB' : 'grape',
+                'objectA'  : {
+                    'oa1'  : 1,
+                    'oa2'  : 2
+                }
+            };
+            fs.readJsonSync.andReturn({
+                'settingB' : 'apple',
+                'settingC' : 'strawberry',
+                'objectA'  : {
+                    'oa2'  : 'dork',
+                    'oa3'  : 'oomph'
+                },
+                'objectB' : {
+                    'v1' : 1,
+                    'v2' : 2
+                }
+            });
             
-        it('should throw an error if given a non existant configuration file', function(){
-            expect(function(){
-                config.createConfigObject('abc.cfg', defaultCfg);
-            }).toThrow('ENOENT, no such file or directory \'abc.cfg\'');
+            var res = config.createConfigObject('tmpcfg.json', defaultCfg);
+            expect(res.settingA).toEqual('banana');
+            expect(res.settingB).toEqual('apple');
+            expect(res.settingC).toEqual('strawberry');
+            expect(res.objectA.oa1).toEqual(1);
+            expect(res.objectA.oa2).toEqual('dork');
+            expect(res.objectA.oa3).toEqual('oomph');
+            expect(res.objectB.v1).toEqual(1);
+            expect(res.objectB.v2).toEqual(2);
         });
 
-        it('should throw an error if given a badly formed configuration file', function(){
-            rmList.push(path.join(__dirname,'tmpcfg.json'));
-            fs.writeFileSync(path.join(__dirname,'tmpcfg.json'),'abc');
-            expect(function(){
-                config.createConfigObject(path.join(__dirname,'tmpcfg.json'), defaultCfg);
-            }).toThrow('Unexpected token a');
-        });
     });
 });
