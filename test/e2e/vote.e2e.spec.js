1a1db225e000b4609d7210b29fae2182db61d6ae
Howard Engelhart <howardengelhart@yahoo.com>
2014-02-04 07:36:36 -0500

    adding e2e tests for vote

---
+++ test/e2e/vote.e2e.spec.js
@@ -0,0 +1,123 @@
+describe('vote (E2E)', function(){
+    var host, port, testUrl, flush, testUtils;
+    beforeEach(function(){
+        if (flush){ for (var m in require.cache){ delete require.cache[m]; } flush = false; }
+        host        = process.env['E2EHOST'] ? process.env['E2EHOST'] : '33.33.33.10';
+        port        = process.env['E2EPORT'] ? process.env['E2EPORT'] : 80;
+        testUtils   = require('./testUtils');
+      
+        if (port === 443){
+            testUrl = 'https://' + host;
+        } else {
+            testUrl = 'http://' + host;
+            if (port !== 80){
+                testUrl += ':' + port;
+            }
+        }
+    });
+
+    describe('GET /election/:id',function(){
+
+        it('gets an election if it exists',function(done){
+            testUtils.qRequest('get', { url : testUrl + '/election/e1'})
+                .then(function(resp){
+                    expect(resp.response.statusCode).toEqual(200);
+                    expect(resp.body.id).toEqual('e1');
+                    expect(resp.body.ballot.b1['red apple']).toEqual(0.17);
+                    expect(resp.body.ballot.b1['yellow banana']).toEqual(0.33);
+                    expect(resp.body.ballot.b1['orange carrot']).toEqual(0.50);
+                    expect(resp.body.ballot.b2['one chicken']).toEqual(0.33);
+                    expect(resp.body.ballot.b2['two ducks']).toEqual(0.67);
+                })
+                .catch(function(err){
+                    expect(err).not.toBeDefined();
+                })
+                .finally(function(){
+                    done();
+                });
+        });
+
+        it('returns with a 404 if the election does not exist',function(done){
+            testUtils.qRequest('get', { url : testUrl + '/election/e1x'})
+                .then(function(resp){
+                    expect(resp.response.statusCode).toEqual(404);
+                })
+                .catch(function(err){
+                    expect(err).not.toBeDefined();
+                })
+                .finally(function(){
+                    done();
+                });
+
+        });
+
+        it('returns with 404 if the electionId is not passed',function(done){
+            testUtils.qRequest('get', { url : testUrl + '/election'})
+                .then(function(resp){
+                    expect(resp.response.statusCode).toEqual(404);
+                    expect(resp.body).toEqual('Cannot GET /election');
+                })
+                .catch(function(err){
+                    expect(err).not.toBeDefined();
+                })
+                .finally(function(){
+                    done();
+                });
+
+        });
+
+    });
+
+
+    describe('GET /election/:id/ballot:id',function(){
+
+        it('gets a ballot if it and the election exist',function(done){
+            testUtils.qRequest('get', { url : testUrl + '/election/e2/ballot/b2'})
+                .then(function(resp){
+                    expect(resp.response.statusCode).toEqual(200);
+                    expect(resp.body.id).toEqual('e2');
+                    expect(resp.body.ballot.b2['red fish']).toEqual(0.43);
+                    expect(resp.body.ballot.b2['blue fish']).toEqual(0.57);
+                })
+                .catch(function(err){
+                    expect(err).not.toBeDefined();
+                })
+                .finally(function(){
+                    done();
+                });
+
+        });
+        
+        it('returns with a 404 if the election does not exist',function(done){
+            testUtils.qRequest('get', { url : testUrl + '/election/e2x/ballot/b2'})
+                .then(function(resp){
+                    expect(resp.response.statusCode).toEqual(404);
+                })
+                .catch(function(err){
+                    expect(err).not.toBeDefined();
+                })
+                .finally(function(){
+                    done();
+                });
+
+        });
+
+        it('returns with a 404 if the ballot does not exist',function(done){
+            testUtils.qRequest('get', { url : testUrl + '/election/e2/ballot/b3'})
+                .then(function(resp){
+                    expect(resp.response.statusCode).toEqual(404);
+                })
+                .catch(function(err){
+                    expect(err).not.toBeDefined();
+                })
+                .finally(function(){
+                    done();
+                });
+
+        });
+
+
+    });
+
+
+});
