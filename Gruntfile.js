f775151c182dfbd5147db6e4df50813e7c628d8a
Evan Leichter <evan.leichter@gmail.com>
2014-03-03 18:19:19 -0500

    Added grunt job for creating EBS snapshots

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,10 @@ module.exports = function (grunt) {
                 tasks: ['jshint', 'unit_tests' ]
             }
         },
+        create_snapshot: {
+            pollingInterval: 5,
+            maxIters: 24
+        },
         stop_instances: {
             pollingInterval: 5,
             maxIters: 24
@@ -316,6 +320,78 @@ module.exports = function (grunt) {
             });
         });
     });
+    
+    grunt.registerTask('create_snapshot', 'create a snapshot of an EBS volume', function(volumeId) {
+        var settings = grunt.config.get('settings'),
+            auth     = settings.awsAuth,
+            interval = grunt.config.get('create_snapshot.pollingInterval') * 1000,
+            maxIters = grunt.config.get('create_snapshot.maxIters'),
+            desc     = grunt.option('description') ||
+                           'Created from ' + volumeId + ' on ' + new Date().toString();
+        
+        if (!volumeId) {
+            grunt.log.errorlns('Need to provide a volumeId');
+            return false;
+        }
+        
+        var done = this.async();
+        aws.config.loadFromPath(auth);
+        var ec2 = new aws.EC2();
+        
+        grunt.log.writelns('Creating snapshot of volume ' + volumeId);
+        ec2.createSnapshot({VolumeId: volumeId, Description: desc}, function(err, data) {
+            if (err) {
+                grunt.log.errorlns(err);
+                return done(false);
+            }
+            if (!data || !data.SnapshotId) {
+                grunt.log.errorlns('Incomplete information from creating the snapshot:');
+                grunt.log.errorlns(JSON.stringify(data));
+                return done(false);
+            }
+            var checkOpts = {
+                id: data.SnapshotId,
+                state: 'completed',
+                interval: interval,
+                maxIters: maxIters
+            };
+            grunt.log.writelns('Created snapshot ' + data.SnapshotId);
+            checkSnapshot(checkOpts, ec2, 0)
+            .then(function(snapshot) {
+                grunt.log.writelns('Snapshot ' + snapshot.SnapshotId + ' is completed');
+                done(true);
+            }).catch(function(error) {
+                grunt.log.errorlns(error);
+                done(false);
+            });
+        });
+    });
+    
+    function checkSnapshot(opts, ec2, iters, promise) {
+        var deferred = promise || q.defer();
+        grunt.log.writelns('Polling snapshot ' + opts.id + ' for its state');
+        ec2.describeSnapshots({SnapshotIds: [opts.id]}, function(err, data) {
+            if (err) {
+                deferred.reject(err);
+                return;
+            }
+            if (!data || !data.Snapshots) {
+                return deferred.reject('Incomplete information from describeSnapshots');
+            }
+            
+            if (data.Snapshots[0].State !== opts.state) {
+                iters++;
+                if (iters >= opts.maxIters) {
+                    deferred.reject('Timed out after ' + iters + ' iterations');
+                    return;
+                }
+                setTimeout(checkSnapshot, opts.interval, opts, ec2, iters, deferred);
+            } else {
+                deferred.resolve(data.Snapshots[0]);
+            }
+        });
+        return deferred.promise;
+    }
 
     function checkState(opts, ec2, iters, promise) {
         var deferred = promise || q.defer(),
