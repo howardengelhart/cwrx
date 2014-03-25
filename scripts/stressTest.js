de42d84876a5d63319789712cdcd672515d6fbad
Evan Leichter <evan.leichter@gmail.com>
2014-03-25 16:41:08 -0400

    Final updates to stressTest.js

--- scripts/stressTest.js
+++ scripts/stressTest.js
@@ -9,7 +9,7 @@
         url     = require('url'),
         q       = require('q');
 
-    function sendRequest(reqOpts) {
+    function sendRequest(reqOpts, reqNum, testId) {
         var start = new Date(),
             deferred = q.defer(),
             code = reqOpts.code || 200,
@@ -18,6 +18,8 @@
                 url: reqOpts.url,
                 data: reqOpts.json
             };
+        resp.reqNum = reqNum;
+        reqOpts.headers = { testId: testId, reqNum: reqNum };
         reqOpts.pool = { maxSockets: 10000 };
         request(reqOpts, function(error, response, body) {
             var end = new Date();
@@ -34,73 +36,48 @@
             resp.code = response.statusCode;
             resp.body = body;
             if (response.statusCode !== code) {
-                resp.error = 'Got ' + response.statusCode + ' instead of ' + reqOpts.code;
+                resp.error = 'Got ' + response.statusCode + ' instead of ' + code;
                 return deferred.reject(resp);
             }
             return deferred.resolve(resp);
         });
         
         return deferred.promise;
-        /*var deferred = q.defer(), opts, start = new Date(), resp, data, req, parsed;
-        parsed = url.parse(reqOpts.url);
-        opts = {
-            hostname : parsed.hostname,
-            port     : parsed.port,
-            path     : parsed.pathname,
-            //hostname   : 'localhost',
-            //port       : 80,
-            //path       : '/share/meta',
-            method   : 'GET',
-            agent    : false
-        };
-        resp = { start: start};
-
-        req = http.request(opts,function(res){
-            resp.code = parseInt(res.statusCode,10);
-            if (resp.code !== 200){
-                resp.error = 'Got ' + resp.code + ' instead of 200';
-                return deferred.reject(resp);
-            }
-            res.setEncoding('utf8');
-            data = '';
-            
-            res.on('data',function(chunk){
-                if (!resp.dataT) resp.dataT = new Date() - resp.start;
-                data += chunk;
-            });
-            res.on('end',function(){
-                resp.end = new Date();
-                resp.elapsed = resp.end - resp.start;
-                resp.body = data;
-                deferred.resolve(resp);
-            });
+    }
+    
+    function dualWrite(msg, stream) {
+        console.log(msg);
+        stream.write(msg + '\n');
+    }
+    
+    function reportPercentiles(list, stream) {
+        dualWrite('---------------------------------', stream);
+        dualWrite('Percentiles:', stream);
+        var percentiles = [0.99, 0.98, 0.97, 0.96, 0.95, 0.9, 0.8, 0.75, 0.5, 0.25, 0.1];
+        list.sort(function(a, b) { return a.elapsed - b.elapsed; });
+        percentiles.forEach(function(percentile) {
+            var bin = Math.round(percentile * list.length) - 1;
+            dualWrite(Math.round(percentile * 100) + '%:\t' + list[bin].elapsed, stream);
         });
-        req.on('socket', function() {
-            resp.socketT = new Date() - start;
-        });
-        req.setNoDelay(true);
-        req.on('error',function(e){
-            resp.end = new Date();
-            resp.elapsed = resp.end - resp.start;
-            resp.error = e;
-            deferred.reject(e);
-        });
-        req.end();
-        return deferred.promise;*/
-        /*var deferred = q.defer(),
-            start = new Date(),
-            resp = { start: start };
-        cp.exec('curl ' + reqOpts.url, function(error, stderr, stdout) {
-            resp.elapsed = new Date() - start;
-            if (error) {
-                resp.error = error;
-                deferred.reject(resp);
+    }
+    
+    function writeResults(successes, failures, stream, verbose) {
+        if (successes.length) stream.write('Successes: \n');
+        stream.write(successes.map(function(s) {
+            return 'ReqNum: ' + s.reqNum + ',  \tStart: ' + s.start.toISOString() + ',   End: ' + s.end.toISOString() +
+                   ',   Elapsed: ' + s.elapsed + ',\tUrl: ' + s.url;
+        }).join('\n'));
+        stream.write('\n----------------------------------------------------------------------\n');
+        if (failures.length) stream.write('Failures: \n');
+        stream.write(failures.map(function(f) {
+            if (program.verbose) {
+                return 'ReqNum: ' + f.reqNum + ',  \tStart: ' + f.start.toISOString() + ',   End: ' + f.end.toISOString() +
+                       ',   Elapsed: ' + f.elapsed + ',\tCode: ' + f.code + ',   Error: ' + f.error + ',   Url: ' + f.url;
             } else {
-                resp.body = stdout.trim();
-                deferred.resolve(resp);
+                return 'ReqNum: ' + f.reqNum + ',  \tStart: ' + f.start.toISOString() + ',   End: ' + f.end.toISOString() +
+                       ',   Elapsed: ' + f.elapsed + ',\tCode: ' + f.code + ',   Url: ' + f.url;
             }
-        });
-        return deferred.promise;*/
+        }).join('\n'));
     }
     
     function avg(arr, prop) {
@@ -142,7 +119,8 @@
             .option('-u, --url [URL]','Url to send requests to')
             .option('-d, --data [JSON]','Stringified JSON data', JSON.parse, {})
             .option('--config [FILE]','Config file with requests to make', fs.readJsonSync)
-            .option('-v, --verbose','Log all error messages')
+            .option('-o, --output [FILE]','File to output info to', 'out.txt')
+            .option('-v, --verbose','Output all error messages')
             .parse(process.argv);
         if (!program.url && !program.config) {
             throw new Error('Must provide a target url or a config file with requests to make');
@@ -150,21 +128,32 @@
         
         var requests = (program.config && program.config.requests) ||
                        [ { url: program.url, json: program.data } ],
+            testId = Math.round(Math.random() * 10000000),
+            stream = fs.createWriteStream(program.output),
             sentReqs = 0, successes = [], failures = [];
-        
+
+        dualWrite('TestId:\t' + testId, stream);
+        if (program.config && program.config.requests) {
+            dualWrite('Test Endpoints:', stream);
+            dualWrite(program.config.requests.map(function(request) { return request.url; }).join('\n'), stream);
+        } else {
+            dualWrite('Test Endpoint:\t' + program.url, stream);
+        }
+        dualWrite('Tests started at ' + new Date().toISOString(), stream);
+        dualWrite('---------------------------------', stream);
         process.nextTick(function sendBatchReqs() {
             var promises = [],
                 reqsToMake = Math.min(program.concurrency, program.numRequests - sentReqs);
 
             for (var i = 0; i < reqsToMake; i++) {
                 var randReq = requests[Math.floor(Math.random() * requests.length)];
-                promises.push(sendRequest(randReq));
+                promises.push(sendRequest(randReq, sentReqs, testId));
                 sentReqs++;
             }
             
             q.allSettled(promises).done(function(results) {
-                console.log('Finished batch of ' + results.length + ' results at ' +
-                            new Date().toISOString());
+                dualWrite('Finished batch of ' + results.length + ' results at ' +
+                            new Date().toISOString(), stream);
                 results.forEach(function(result) {
                     if (result.state === 'fulfilled') {
                         successes.push(result.value);
@@ -183,31 +172,25 @@
                 failures.name = 'Failures';
                 allResponses.name = 'All Responses';
                 [successes, failures, allResponses].forEach(function(list) {
-                    console.log('---------------------------------');
-                    console.log(list.name + ':');
-                    console.log('Number: ' + list.length);
+                    dualWrite('---------------------------------', stream);
+                    dualWrite(list.name + ':', stream);
+                    dualWrite('Number: ' + list.length, stream);
                     if (list.length === 0) {
                         return;
                     }
-                    if (list.name === 'All Responses' && (list.length === successes.length ||
+                    if (list.name === 'All Responses' && (list.length === successes.length || 
                                                           list.length === failures.length)) {
                         return;
                     }
-                    console.log('Average time: ' + avg(list));
+                    dualWrite('Average time:\t' + avg(list), stream);
                     var minTime = min(list), maxTime = max(list);
-                    console.log('Best time: \t' + minTime.elapsed + ',\t' + ' started at: ' +
-                                minTime.start.toISOString());
-                    console.log('Worst time: \t' + maxTime.elapsed + ',\t' + ' started at: ' + 
-                                maxTime.start.toISOString());
-                    console.log('Average socketT: \t' + avg(list, 'socketT'));
-                    console.log('Average dataT: \t' + avg(list, 'dataT'));
-                    console.log('Average connectT: \t' + avg(list, 'connectT'));
-                    if (list.name === 'Failures' && program.verbose) {
-                        list.forEach(function(failure) {
-                            console.log(JSON.stringify(failure));
-                        });
-                    }
+                    dualWrite('Best time:\t\t' + minTime.elapsed + ',\t' + ' ReqNum: ' + minTime.reqNum, stream);
+                    dualWrite('Worst time:\t\t' + maxTime.elapsed + ',\t' + ' ReqNum: ' + maxTime.reqNum, stream);
                 });
+                reportPercentiles(allResponses, stream);
+                dualWrite('---------------------------------', stream);
+                writeResults(successes, failures, stream, program.verbose);
+                stream.end();
             });
         });
     }
