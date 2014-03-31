18edc0aac43c4d27c95329fd8ca93497b17f671a
Evan Leichter <evan.leichter@gmail.com>
2014-03-31 17:42:42 -0400

    authUtils now uses QueryCache

--- test/unit/authUtils.spec.js
+++ test/unit/authUtils.spec.js
@@ -7,6 +7,7 @@ describe('authUtils', function() {
         q           = require('q');
         authUtils   = require('../../lib/authUtils')();
         mongoUtils  = require('../../lib/mongoUtils');
+        QueryCache  = require('../../lib/queryCache');
         logger      = require('../../lib/logger');
         uuid        = require('../../lib/uuid');
         
@@ -22,122 +23,102 @@ describe('authUtils', function() {
                 }
             }
         };
+        
+        spyOn(authUtils, '_initCache').andCallThrough();
+        authUtils._cache = new QueryCache(5, 10, 'fakeColl');
+    });
+    
+    describe('initialization', function() {
+        it('should initialize the cache if provided with a collection', function() {
+            authUtils = require('../../lib/authUtils')(1, 2, 'fakeColl');
+            expect(authUtils._cache).toBeDefined();
+            expect(authUtils._cache.freshTTL).toBe(1*60*1000);
+            expect(authUtils._cache.maxTTL).toBe(2*60*1000);
+            expect(authUtils._cache._coll).toBe('fakeColl');
+        });
+        
+        it('should use defaults for the TTLs if not provided', function() {
+            authUtils = require('../../lib/authUtils')(null, null, 'fakeColl');
+            expect(authUtils._cache).toBeDefined();
+            expect(authUtils._cache.freshTTL).toBe(5*60*1000);
+            expect(authUtils._cache.maxTTL).toBe(10*60*1000);
+            expect(authUtils._cache._coll).toBe('fakeColl');
+        });
+    });
+    
+    describe('_initCache', function() {
+        beforeEach(function() {
+            delete authUtils._cache;
+        });
+        
+        it('should just return if the cache is defined', function() {
+            authUtils._cache = 'fakeCache';
+            expect(authUtils._initCache).not.toThrow();
+        });
+        
+        it('should initialize the cache if not defined and a collection is provided', function() {
+            expect(authUtils._cache).not.toBeDefined();
+            authUtils._initCache('fakeColl');
+            expect(authUtils._cache).toBeDefined();
+            expect(authUtils._cache.freshTTL).toBe(5*60*1000);
+            expect(authUtils._cache.maxTTL).toBe(10*60*1000);
+            expect(authUtils._cache._coll).toBe('fakeColl');
+        });
+        
+        it('should throw an error if the cache is undefined and no collection is provided', function() {
+            expect(authUtils._initCache).toThrow('Cannot call this method without providing a collection');
+        });
     });
     
     describe('getUser', function() {
-        var collection, db;
+        var collection;
         beforeEach(function() {
-            jasmine.Clock.useMock();
-            authUtils._cache = {};
-            collection = {
-                findOne: jasmine.createSpy('coll_findOne').andCallFake(function(query, cb) {
-                    cb(null, mockUser);
-                })
-            };
-            db = {
-                collection: jasmine.createSpy('db_coll').andReturn(collection)
-            };
+            collection = 'fakeColl';
+            spyOn(authUtils._cache, 'getPromise').andReturn(q([mockUser]));
             spyOn(mongoUtils, 'safeUser').andCallThrough();
         });
         
-        it('should retrieve a user from the cache if they exist', function(done) {
-            authUtils._cache['u-1234'] = mockUser;
-            authUtils.getUser('u-1234', db).then(function(user) {
-                expect(user).toEqual(mockUser);
-                expect(collection.findOne).not.toHaveBeenCalled();
-                done();
-            }).catch(function(error) {
-                expect(error).not.toBeDefined();
-                done();
-            });
-        });
-
-        it('should retrieve from a provided cache if the module was called with one', function(done) {
-            var cache = {
-                'u-1234': mockUser
-            };
-            authUtils = require('../../lib/authUtils')(null, cache);
-            authUtils.getUser('u-1234', db).then(function(user) {
-                expect(user).toEqual(mockUser);
-                expect(collection.findOne).not.toHaveBeenCalled();
-                done();
-            }).catch(function(error) {
-                expect(error).not.toBeDefined();
-                done();
-            });
-        });
-        
-        it('should retrieve a user from mongodb if not in the cache', function(done) {
-            authUtils.getUser('u-1234', db).then(function(user) {
+        it('should call cache.getPromise to get the results of a query', function(done) {
+            authUtils.getUser('u-1234', 'fakeColl').then(function(user) {
                 delete mockUser.password;
                 expect(user).toEqual(mockUser);
-                expect(authUtils._cache['u-1234']).toEqual(mockUser);
-                expect(collection.findOne).toHaveBeenCalled();
-                expect(collection.findOne.calls[0].args[0]).toEqual({id: 'u-1234'});
+                expect(authUtils._initCache).toHaveBeenCalledWith('fakeColl');
+                expect(authUtils._cache.getPromise).toHaveBeenCalledWith({id: 'u-1234'});
                 expect(mongoUtils.safeUser).toHaveBeenCalled();
                 done();
             }).catch(function(error) {
-                expect(error).not.toBeDefined();
+                expect(error.toString()).not.toBeDefined();
                 done();
             });
         });
         
-        it('should resolve with null if the user cannot be found', function(done) {
-            collection.findOne.andCallFake(function(query, cb) {
-                cb(null, null);
-            });
-            authUtils.getUser('u-1234', db).then(function(user) {
-                expect(user).toBeNull();
-                expect(authUtils._cache['u-1234']).not.toBeDefined();
-                expect(collection.findOne).toHaveBeenCalled();
+        it('should resolve with nothing if no results are found', function(done) {
+            authUtils._cache.getPromise.andReturn(q());
+            authUtils.getUser('u-1234').then(function(user) {
+                expect(user).not.toBeDefined();
+                authUtils._cache.getPromise.andReturn(q([]));
+                return authUtils.getUser('u-1234');
+            }).then(function(user) {
+                expect(user).not.toBeDefined();
+                authUtils._cache.getPromise.andReturn(q('this is not a user'));
+                return authUtils.getUser('u-1234');
+            }).then(function(user) {
+                expect(user).not.toBeDefined();
                 expect(mongoUtils.safeUser).not.toHaveBeenCalled();
                 done();
             }).catch(function(error) {
-                expect(error).not.toBeDefined();
+                expect(error.toString()).not.toBeDefined();
                 done();
             });
         });
         
-        it('should reject if mongo encounters an error', function(done) {
-            collection.findOne.andCallFake(function(query, cb) {
-                cb('Error!');
-            });
-            authUtils.getUser('u-1234', db).catch(function(errorObj) {
-                expect(errorObj).toBeDefined();
-                expect(errorObj.error).toBe("Error looking up user");
-                expect(errorObj.detail).toBe('Error!');
-                expect(collection.findOne).toHaveBeenCalled();
-                done();
-            });
-        });
-        
-        it('should delete items after a period of time', function(done) {
-            authUtils.getUser('u-1234', db).then(function(user) {
-                delete mockUser.password;
-                expect(user).toEqual(mockUser);
-                expect(authUtils._cache['u-1234']).toEqual(mockUser);
-                jasmine.Clock.tick(31*60*1000);
-                expect(authUtils._cache['u-1234']).not.toBeDefined();
-                done();
+        it('should pass on errors from the cache', function(done) {
+            authUtils._cache.getPromise.andReturn(q.reject('I GOT A PROBLEM'));
+            authUtils.getUser('u-1234').then(function(user) {
+                expect(user).not.toBeDefined();
             }).catch(function(error) {
-                expect(error).not.toBeDefined();
-                done();
-            });
-        });
-        
-        it('should have a configurable cache TTL', function(done) {
-            authUtils = require('../../lib/authUtils')(60);
-            authUtils.getUser('u-1234', db).then(function(user) {
-                delete mockUser.password;
-                expect(user).toEqual(mockUser);
-                expect(authUtils._cache['u-1234']).toEqual(mockUser);
-                jasmine.Clock.tick(31*60*1000);
-                expect(authUtils._cache['u-1234']).toBeDefined();
-                jasmine.Clock.tick(30*60*1000);
-                expect(authUtils._cache['u-1234']).not.toBeDefined();
-                done();
-            }).catch(function(error) {
-                expect(error).not.toBeDefined();
+                expect(error).toBe(JSON.stringify({error: 'Error looking up user', detail: 'I GOT A PROBLEM'}));
+                expect(authUtils._cache.getPromise).toHaveBeenCalledWith({id: 'u-1234'});
                 done();
             });
         });
@@ -192,13 +173,12 @@ describe('authUtils', function() {
             });
         });
         
-        it('should succeed if the user is found and the permissions match', function(done) {
-            authUtils.authUser('u-1234', db, perms).then(function(user) {
+        it('should return a user if found and the permissions match', function(done) {
+            authUtils.authUser('u-1234', perms, 'fakeColl').then(function(result) {
                 delete mockUser.password;
-                expect(user).toEqual(mockUser);
-                expect(authUtils.getUser).toHaveBeenCalled();
-                expect(authUtils.getUser.calls[0].args[0]).toBe('u-1234');
-                expect(authUtils.getUser.calls[0].args[1]).toBe('mockDB');
+                expect(result.user).toEqual(mockUser);
+                expect(authUtils._initCache).toHaveBeenCalledWith('fakeColl');
+                expect(authUtils.getUser).toHaveBeenCalledWith('u-1234');
                 expect(authUtils.compare).toHaveBeenCalledWith(perms, mockUser.permissions);
                 done();
             }).catch(function(error) {
@@ -207,37 +187,40 @@ describe('authUtils', function() {
             });
         });
         
-        it('should fail the permissions do not match', function(done) {
+        it('should return a fail message if the permissions do not match', function(done) {
             authUtils.compare.andReturn(false);
-            authUtils.authUser('u-1234', db, perms).catch(function(errorObj) {
-                expect(errorObj).toBeDefined();
-                expect(errorObj.error).toBe("Permissions do not match");
-                expect(errorObj.detail).not.toBeDefined();
-                expect(authUtils.getUser).toHaveBeenCalled();
+            authUtils.authUser('u-1234', perms).then(function(result) {
+                expect(result).toBe('Permissions do not match');
+                expect(result.user).not.toBeDefined();
+                expect(authUtils.getUser).toHaveBeenCalledWith('u-1234');
+                expect(authUtils.compare).toHaveBeenCalledWith(perms, mockUser.permissions);
+                done();
+            }).catch(function(error) {
+                expect(error).not.toBeDefined();
                 done();
             });
         });
         
-        it('should fail if the user is not active', function(done) {
+        it('should return a fail message if the user is not active', function(done) {
             mockUser.status = 'inactive';
-            authUtils.authUser('u-1234', db, perms).catch(function(errorObj) {
-                expect(errorObj).toBeDefined();
-                expect(errorObj.error).toBe("User is inactive");
-                expect(errorObj.detail).not.toBeDefined();
-                expect(authUtils.getUser).toHaveBeenCalled();
+            authUtils.authUser('u-1234', perms).then(function(result) {
+                expect(result).toBe('User is not active');
+                expect(result.user).not.toBeDefined();
+                expect(authUtils.getUser).toHaveBeenCalledWith('u-1234');
+                expect(authUtils.compare).not.toHaveBeenCalled();
+                done();
+            }).catch(function(error) {
+                expect(error).not.toBeDefined();
                 done();
             });
         });
         
         it('should fail if getting the user fails', function(done) {
-            authUtils.getUser.andReturn(q.reject({
-                error: 'Error!',
-                detail: 'NOPE'
-            }));
-            authUtils.authUser('u-1234', db, perms).catch(function(errorObj) {
-                expect(errorObj).toBeDefined();
-                expect(errorObj.error).toBe("Error!");
-                expect(errorObj.detail).toBe("NOPE");
+            authUtils.getUser.andReturn(q.reject('I GOT A PROBLEM'));
+            authUtils.authUser('u-1234', db, perms).then(function(result) {
+                expect(result).not.toBeDefined();
+            }).catch(function(error) {
+                expect(error).toBe('I GOT A PROBLEM');
                 expect(authUtils.getUser).toHaveBeenCalled();
                 expect(authUtils.compare).not.toHaveBeenCalled();
                 done();
@@ -246,16 +229,15 @@ describe('authUtils', function() {
     });
     
     describe('middlewarify: ', function() {
-        
         it('should return a function', function() {
-            var midWare = authUtils.middlewarify('fakeDb', 'fakePerms');
+            var midWare = authUtils.middlewarify('fakePerms', 'fakeColl');
+            expect(authUtils._initCache).toHaveBeenCalledWith('fakeColl');
             expect(typeof midWare).toBe('function');
         });
         
         describe('middleware', function() {
-            var db, perms, mockLog, req, res, next;
+            var perms, mockLog, req, res, next;
             beforeEach(function() {
-                db = "mockDb";
                 perms = "fakePerms";
                 mockLog = {
                     trace : jasmine.createSpy('log_trace'),
@@ -278,11 +260,11 @@ describe('authUtils', function() {
                     }
                 };
                 res = {};
-                spyOn(authUtils, 'authUser').andReturn(q({ id: 'u-123' }));
+                spyOn(authUtils, 'authUser').andReturn(q({ user: { id: 'u-123' } }));
             });
         
             it('should correctly wrap authUser', function(done) {
-                var midWare = authUtils.middlewarify(db, perms);
+                var midWare = authUtils.middlewarify(perms);
                 res.send = function(code, data) {
                     expect(code).not.toBeDefined();
                     expect(data).not.toBeDefined();
@@ -293,14 +275,14 @@ describe('authUtils', function() {
                     expect(mockLog.info).toHaveBeenCalled();
                     expect(req.uuid).toBe('1234');
                     expect(uuid.createUuid).not.toHaveBeenCalled();
-                    expect(authUtils.authUser).toHaveBeenCalledWith('u-123', 'mockDb', 'fakePerms');
+                    expect(authUtils.authUser).toHaveBeenCalledWith('u-123', 'fakePerms');
                     done();
                 });
             });
             
             it('should call createUuid if there is no req.uuid', function(done) {
                 delete req.uuid;
-                var midWare = authUtils.middlewarify(db, perms);
+                var midWare = authUtils.middlewarify(perms);
                 res.send = function(code, data) {
                     expect(code).not.toBeDefined();
                     expect(data).not.toBeDefined();
@@ -315,7 +297,7 @@ describe('authUtils', function() {
             
             it('should fail with a 401 if there is no user in the session', function(done) {
                 delete req.session.user;
-                var midWare = authUtils.middlewarify(db, perms);
+                var midWare = authUtils.middlewarify(perms);
                 res.send = function(code, data) {
                     expect(code).toBe(401);
                     expect(data).toBe("Unauthorized");
@@ -333,8 +315,8 @@ describe('authUtils', function() {
             });
             
             it('should fail with a 403 if the user is unauthorized', function(done) {
-                authUtils.authUser.andReturn(q.reject({error: 'Error!'}));
-                var midWare = authUtils.middlewarify(db, perms);
+                authUtils.authUser.andReturn(q("HE DON'T BELONG HERE"));
+                var midWare = authUtils.middlewarify(perms);
                 res.send = function(code, data) {
                     expect(code).toBe(403);
                     expect(data).toBe("Forbidden");
@@ -352,8 +334,8 @@ describe('authUtils', function() {
             });
             
             it('should fail with a 500 if there was an internal error', function(done) {
-                authUtils.authUser.andReturn(q.reject({error: 'Error!', detail: 'It broke!'}));
-                var midWare = authUtils.middlewarify(db, perms);
+                authUtils.authUser.andReturn(q.reject('I GOT A PROBLEM'));
+                var midWare = authUtils.middlewarify(perms);
                 res.send = function(code, data) {
                     expect(code).toBe(500);
                     expect(data).toBe("Error checking authorization of user");
