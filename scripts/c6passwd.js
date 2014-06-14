f68c9e25a256c5ad37f48de76d4f6cb5ca1bb144
Howard Engelhart <howard.engelhart@outlook.com>
2014-06-14 15:57:37 -0400

    new script for resetting c6 passwords

---
+++ scripts/c6passwd.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+var bcrypt    = require('bcrypt'),
+    spawn     = require('child_process').spawn,
+    fs        = require('fs'),
+    os        = require('os'),
+    path      = require('path'),
+    cmdl      = require('commander'),
+    makeid    = require('../lib/uuid').createUuid,
+    dbserver  = 'dv-mongo1.corp.cinema6.com',
+    dbuser    = '',
+    user      = '',
+    password  = 'Password1',
+    lastUpdated = (new Date()).toISOString(),
+    script, scriptPath, cmdl;
+
+
+cmdl
+    .option('--db-server <servername>'  , 'Db server (' + dbserver + ')', dbserver)
+    .option('--db-user <logonid>'       , 'User name for database.')
+    .option('--password <password>'     , 'Reset Cinema6 password (' + password + ')',password)
+    .option('--user <username>'         , 'Cinema6 username')
+    .parse(process.argv);
+
+dbserver = cmdl.dbServer;
+dbuser   = cmdl.dbUser;
+user     = cmdl.user;
+password = bcrypt.hashSync(cmdl.password,bcrypt.genSaltSync());
+
+script = [  'c6Db=db.getSiblingDB("c6Db");',
+                'c6Db.users.update({email:"' + user + '"},',
+                '{$set:{password:"' + password + '",',
+                'lastUpdated:"' + lastUpdated + '"}});',
+                'cursor=c6Db.users.find({email:"' + user + '"});',
+                'printjson( cursor.next() );'
+            ].join('');
+
+scriptPath = path.join(os.tmpdir(),makeid().substr(0,14) + '.js');
+
+fs.writeFileSync(scriptPath,script);
+spawn('mongo',[dbserver + '/admin',scriptPath,'-u',dbuser,'-p'],{ stdio : 'inherit' })
+    .on('close',function(){
+        fs.unlinkSync(scriptPath);
+        process.exit();
+    });
+
+
+
