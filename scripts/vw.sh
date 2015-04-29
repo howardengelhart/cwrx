f5bc1db1acca195500c5d794267dd531875b571c
Evan Leichter <evan.leichter@gmail.com>
2015-04-29 17:31:21 -0400

    remove lots of old unused code + tests

--- scripts/vw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-index=1
-vwarepath=$1
-for voice in "Large Male" "Giant Male" "Male" "Female" "Child" "Old Woman" "Robotoid" "Martian" "Munchkin" "Colossus" "MellowFemale" "MellowMale" "CrispMale" "TheFly" "FastFred" "Troll" "Nerd" "MilkToast" "Tipsy" "Choirboy"
-do
-    echo "This is voice ${index}, ${voice}"
-    $vwarepath -o eid=6 -o vid=${index}  \""This is voice ${index}, ${voice} "\"
-    ((index++))
-done
