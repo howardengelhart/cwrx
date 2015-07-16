96255babdc79bda8ff4bc4a91fa34a529ed7598d
Joshua Minzner <josh@minzner.org>
2015-07-16 14:02:51 -0400

    Add promise.Timer() for rejecting multiple promises if a single timer expires.

--- test/unit/promise.spec.js
+++ test/unit/promise.spec.js
@@ -1,9 +1,10 @@
 
 describe('promise',function(){
-    var flush = true, promise;
+    var flush = true, promise, q;
     beforeEach(function() {
         if (flush){ for (var m in require.cache){ delete require.cache[m]; } flush = false; }
         promise = require('../../lib/promise');
+        q = require('q');
         jasmine.Clock.useMock();
     });
 
@@ -277,6 +278,292 @@ describe('promise',function(){
                     expect(rejectSpy.argsForCall[0][0]).toEqual('abc');
                     expect(rejectSpy.argsForCall[1][0]).toEqual('ghi');
                     done();
+                });
+            });
+        });
+    });
+
+    describe('Timer(time)', function() {
+        var timer;
+        var time;
+
+        beforeEach(function() {
+            time = 500;
+            timer = new promise.Timer(time);
+        });
+
+        it('should exist', function() {
+            expect(timer).toEqual(jasmine.any(Object));
+        });
+
+        describe('properties:', function() {
+            describe('expired', function() {
+                it('should be false', function() {
+                    expect(timer.expired).toBe(false);
+                });
+
+                it('should not be settable', function() {
+                    expect(function() { timer.expired = true; }).toThrow();
+                });
+            });
+        });
+
+        describe('methods:', function() {
+            var WAIT_TIME = 10;
+
+            describe('watch(promise)', function() {
+                var deferred;
+                var success, failure;
+                var result;
+
+                beforeEach(function() {
+                    deferred = q.defer();
+                    success = jasmine.createSpy('success()');
+                    failure = jasmine.createSpy('failure()');
+
+                    jasmine.Clock.tick(WAIT_TIME);
+
+                    result = timer.watch(deferred.promise);
+                    result.then(success, failure);
+                });
+
+                it('should return a new promise', function() {
+                    expect(result).toEqual(jasmine.any(q().constructor));
+                    expect(result).not.toBe(deferred.promise);
+                });
+
+                describe('if the provided promise is fulfilled', function() {
+                    var value;
+
+                    beforeEach(function(done) {
+                        value = { foo: 'bar' };
+
+                        jasmine.Clock.tick(time - WAIT_TIME - 1);
+                        deferred.resolve(value);
+                        q().then(done);
+                    });
+
+                    it('should fulfill the returned promise', function() {
+                        expect(success).toHaveBeenCalledWith(value);
+                    });
+                });
+
+                describe('if the provided promise is rejected', function() {
+                    var reason;
+
+                    beforeEach(function(done) {
+                        reason = new Error('I suck.');
+
+                        jasmine.Clock.tick(time - WAIT_TIME - 1);
+                        deferred.reject(reason);
+                        q().then(done);
+                    });
+
+                    it('should reject the returned promise', function() {
+                        expect(failure).toHaveBeenCalledWith(reason);
+                    });
+                });
+
+                describe('if the timer expires', function() {
+                    beforeEach(function(done) {
+                        jasmine.Clock.tick((time - WAIT_TIME) + 1);
+                        deferred.resolve('foo');
+
+                        q().then(done);
+                    });
+
+                    it('should set expired to true', function() {
+                        expect(timer.expired).toBe(true);
+                    });
+
+                    it('should reject the promise with a timeout error', function() {
+                        expect(failure).toHaveBeenCalledWith(jasmine.any(Error));
+                        var error = failure.mostRecentCall.args[0];
+
+                        expect(error.message).toBe('Timed out after ' + time + ' ms');
+                        expect(error.code).toBe('ETIMEDOUT');
+                    });
+
+                    describe('if another promise is watched', function() {
+                        beforeEach(function(done) {
+                            failure.reset();
+                            success.reset();
+
+                            timer.watch(q({})).then(success, failure);
+                            q().then(done);
+                        });
+
+                        it('should reject the promise', function() {
+                            expect(failure).toHaveBeenCalledWith(jasmine.any(Error));
+
+                            var error = failure.mostRecentCall.args[0];
+                            expect(error.message).toBe('Timed out after ' + time + ' ms');
+                            expect(error.code).toBe('ETIMEDOUT');
+                        });
+                    });
+                });
+
+                describe('when called with different promises', function() {
+                    var deferred1, deferred2, deferred3;
+                    var deferreds;
+                    var failure1, failure2, failure3;
+                    var failures;
+
+                    beforeEach(function() {
+                        deferred1 = q.defer();
+                        deferred2 = q.defer();
+                        deferred3 = q.defer();
+                        deferreds = [deferred1, deferred2, deferred3];
+
+                        failure1 = jasmine.createSpy('failure1()');
+                        failure2 = jasmine.createSpy('failure2()');
+                        failure3 = jasmine.createSpy('failure3()');
+                        failures = [failure1, failure2, failure3];
+
+                        time = 100;
+                        timer = new promise.Timer(time);
+
+                        failures.forEach(function(spy, index) {
+                            var promise = deferreds[index].promise;
+
+                            timer.watch(promise).catch(spy);
+                        });
+                    });
+
+                    describe('and the timer expires', function() {
+                        beforeEach(function(done) {
+                            jasmine.Clock.tick(time + 1);
+                            q().then(done);
+                        });
+
+                        it('should reject all the promises', function() {
+                            failures.forEach(function(spy) {
+                                expect(spy).toHaveBeenCalledWith(jasmine.any(Error));
+                            });
+                        });
+
+                        it('should clean up references to the deferreds', function() {
+                            expect(timer.__private__.deferreds.length).toBe(0);
+                        });
+                    });
+
+                    describe('and a promise fulfills', function() {
+                        var firstDeferred;
+
+                        beforeEach(function(done) {
+                            firstDeferred = timer.__private__.deferreds[0];
+                            deferred1.resolve({});
+
+                            q().then(done);
+                        });
+
+                        it('should remove the reference to that promise\'s deferred', function() {
+                            expect(timer.__private__.deferreds.length).toBe(2);
+                            expect(timer.__private__.deferreds).not.toContain(firstDeferred);
+                        });
+                    });
+
+                    describe('and a promise rejects', function() {
+                        var secondDeferred;
+
+                        beforeEach(function(done) {
+                            secondDeferred = timer.__private__.deferreds[1];
+                            deferred2.reject(new Error());
+
+                            q().then(done);
+                        });
+
+                        it('should remove the reference to that promise\'s deferred', function() {
+                            expect(timer.__private__.deferreds.length).toBe(2);
+                            expect(timer.__private__.deferreds).not.toContain(secondDeferred);
+                        });
+                    });
+                });
+            });
+
+            describe('wrap(fn)', function() {
+                var fn;
+                var result;
+
+                beforeEach(function() {
+                    fn = jasmine.createSpy('fn()');
+
+                    result = timer.wrap(fn);
+                });
+
+                it('should return a function', function() {
+                    expect(result).toEqual(jasmine.any(Function));
+                });
+
+                describe('when the returned function is called', function() {
+                    var arg;
+                    var watchPromise;
+                    var fnResult;
+
+                    beforeEach(function() {
+                        arg = { foo: 'bar' };
+                        watchPromise = q.defer().promise;
+
+                        spyOn(timer, 'watch').andReturn(watchPromise);
+                    });
+
+                    describe('if fn() returns a promise', function() {
+                        var value;
+                        var promise;
+
+                        beforeEach(function(done) {
+                            value = { val: 'hello' };
+                            promise = q(value);
+                            fn.andReturn(promise);
+
+                            fnResult = result(arg);
+                            q().then(done);
+                        });
+
+                        it('should call the provided fn with the supplied args', function() {
+                            expect(fn).toHaveBeenCalledWith(arg);
+                        });
+
+                        it('should watch() the returned promise', function(done) {
+                            expect(timer.watch).toHaveBeenCalledWith(jasmine.any(q().constructor));
+
+                            q.all([promise, timer.watch.mostRecentCall.args[0]]).then(function(vals) {
+                                expect(vals[0]).toBe(vals[1]);
+                            }).done(done);
+                        });
+
+                        it('should return the promise returned by watch()', function() {
+                            expect(fnResult).toBe(watchPromise);
+                        });
+                    });
+
+                    describe('if fn() does not return a promise', function() {
+                        var value;
+
+                        beforeEach(function(done) {
+                            value = { value: 'value' };
+                            fn.andReturn(value);
+
+                            fnResult = result(arg);
+                            q().then(done);
+                        });
+
+                        it('should call the provided fn with the supplied args', function() {
+                            expect(fn).toHaveBeenCalledWith(arg);
+                        });
+
+                        it('should watch() a promise that fulfills with the return value', function(done) {
+                            expect(timer.watch).toHaveBeenCalledWith(jasmine.any(q().constructor));
+
+                            timer.watch.mostRecentCall.args[0].then(function(val) {
+                                expect(val).toBe(value);
+                            }).done(done);
+                        });
+
+                        it('should return the promise returned by watch()', function() {
+                            expect(fnResult).toBe(watchPromise);
+                        });
+                    });
                 });
             });
         });
