98234a9c1208bdce1a3c1fcff4219f641c74decf
Joshua Minzner <josh@minzner.org>
2015-11-18 16:40:43 -0500

    Add collateral method/endpoint for scraping website data with spidey.js.

--- bin/collateral.js
+++ bin/collateral.js
@@ -12,6 +12,7 @@
         glob            = require('glob'),
         handlebars      = require('handlebars'),
         request         = require('request-promise'),
+        spidey          = require('spidey.js'),
         express         = require('express'),
         bodyParser      = require('body-parser'),
         multer          = require('multer'),
@@ -517,7 +518,78 @@
             .finally(cleanup)
             .catch(handleError);
     };
-    
+
+    collateral.getWebsiteData = function getWebsiteData(req, config) {
+        return q().then(function callSpidey() {
+            var log = logger.getLog();
+            var uuid = req.uuid;
+            var uri = req.query.uri;
+
+            if (!uri) {
+                log.info('[%1] Client did not specify a URI.', uuid);
+                return new ServiceResponse(
+                    400,
+                    'Must specify a URI.'
+                );
+            }
+
+            log.info('[%1] Attempting to scrape "%2."', uuid, uri);
+
+            return spidey(uri, {
+                timeout: config.scraper.timeout,
+                gzip: true,
+                headers: { 'User-Agent': config.scraper.agent }
+            }).then(function createResponse(data) {
+                log.info('[%1] Successfully scraped data for "%2."', uuid, uri);
+                return new ServiceResponse(200, data);
+            }).catch(function handleRejection(reason) {
+                var name = reason.name;
+                var cause = reason.cause;
+
+                if (name === 'StatusCodeError') {
+                    log.info('[%1] Upstream server responded with [%2].', uuid, reason.statusCode);
+                    return new ServiceResponse(
+                        400,
+                        'Upstream server responded with status code [' + reason.statusCode + '].'
+                    );
+                }
+
+                if (name === 'RequestError') {
+                    if (cause.code === 'ETIMEDOUT') {
+                        log.warn('[%1] Timed out GETting "%2."', uuid, uri);
+                        return new ServiceResponse(
+                            408,
+                            'Timed out scraping website [' + uri + '].'
+                        );
+                    }
+
+                    if (/Invalid URI/.test(cause.message)) {
+                        log.info('[%1] URI is not valid: %2.', uuid, uri);
+                        return new ServiceResponse(
+                            400,
+                            'URI [' + uri + '] is not valid.'
+                        );
+                    }
+
+                    log.warn('[%1] Unexpected Error from request: %2.', uuid, util.inspect(cause));
+                    return new ServiceResponse(
+                        500,
+                        'Unexpected error fetching website: ' + util.inspect(reason)
+                    );
+                }
+
+                log.error(
+                    '[%1] Unexpected Error scraping URI [%2]: %3.',
+                    uuid, uri, util.inspect(reason)
+                );
+                return new ServiceResponse(
+                    500,
+                    'Internal error: ' + util.inspect(reason)
+                );
+            });
+        });
+    };
+
     // If num > 6 return 6, else return num
     collateral.chooseTemplateNum = function(num) {
         return Math.min(num, 6);
@@ -961,6 +1033,22 @@
         app.post('/api/collateral/uri', setJobTimeout, sessWrap, authUpload, audit,
             function(req, res) {
                 var promise = q.when(collateral.importFile(req, s3, state.config));
+
+                promise.finally(function() {
+                    return jobManager.endJob(req, res, promise.inspect())
+                        .catch(function(error) {
+                            res.send(500, {
+                                error: 'Error uploading files',
+                                detail: error
+                            });
+                        });
+                });
+            }
+        );
+
+        app.get('/api/collateral/website-data', setJobTimeout, sessWrap, authUpload, audit,
+            function(req, res) {
+                var promise = q.when(collateral.getWebsiteData(req, state.config));
 
                 promise.finally(function() {
                     return jobManager.endJob(req, res, promise.inspect())
--- test/unit/collateral.svc.spec.js
+++ test/unit/collateral.svc.spec.js
@@ -1,13 +1,31 @@
-var flush = true;
 describe('collateral (UT):', function() {
-    var mockLog, uuid, logger, collateral, q, glob, phantom, handlebars, path, enums, Scope, anyFunc, os;
-    var request;
+    var mockLog, uuid, logger, collateral, q, glob, phantom, handlebars, path, enums, Scope, anyFunc, os, util;
+    var request, spidey;
     var EventEmitter;
     var Promise;
+
+    function responseDefer() {
+        var deferred = q.defer();
+        var promise = deferred.promise;
+
+        promise.pipe = jasmine.createSpy('EventedPromise.pipe()');
+        promise.abort = jasmine.createSpy('EventedPromise.abort()');
+
+        EventEmitter.call(promise);
+        for (var method in EventEmitter.prototype) {
+            promise[method] = EventEmitter.prototype[method];
+        }
+
+        return deferred;
+    }
+
+    beforeAll(function() {
+        for (var m in require.cache){ delete require.cache[m]; }
+
+        require('spidey.js');
+    });
     
     beforeEach(function() {
-        if (flush) { for (var m in require.cache){ delete require.cache[m]; } flush = false; }
-
         jasmine.clock().install();
 
         EventEmitter= require('events').EventEmitter;
@@ -21,10 +39,10 @@ describe('collateral (UT):', function() {
         glob        = require('glob');
         handlebars  = require('handlebars');
         fs          = require('fs-extra');
-        collateral  = require('../../bin/collateral');
         q           = require('q');
         Promise     = q.Promise;
         enums       = require('../../lib/enums');
+        util        = require('util');
         Scope       = enums.Scope;
 
         mockLog = {
@@ -39,6 +57,11 @@ describe('collateral (UT):', function() {
         spyOn(logger, 'createLog').and.returnValue(mockLog);
         spyOn(logger, 'getLog').and.returnValue(mockLog);
         anyFunc = jasmine.any(Function);
+
+        spidey = spyOn(require.cache[require.resolve('spidey.js')], 'exports');
+
+        delete require.cache[require.resolve('../../bin/collateral')];
+        collateral  = require('../../bin/collateral');
     });
 
     afterEach(function() {
@@ -182,8 +205,6 @@ describe('collateral (UT):', function() {
     });
 
     describe('importFile(req, s3, config)', function() {
-        var responseDefer;
-
         var req, s3, config;
         var jobId;
         var promise;
@@ -202,21 +223,6 @@ describe('collateral (UT):', function() {
 
             spyOn(fs, 'createWriteStream');
             spyOn(fs, 'remove');
-
-            responseDefer = function responseDefer() {
-                var deferred = q.defer();
-                var promise = deferred.promise;
-
-                promise.pipe = jasmine.createSpy('EventedPromise.pipe()');
-                promise.abort = jasmine.createSpy('EventedPromise.abort()');
-
-                EventEmitter.call(promise);
-                for (var method in EventEmitter.prototype) {
-                    promise[method] = EventEmitter.prototype[method];
-                }
-
-                return deferred;
-            };
 
             success = jasmine.createSpy('success()');
             failure = jasmine.createSpy('failure()');
@@ -840,6 +846,228 @@ describe('collateral (UT):', function() {
                 it('should log a warning', function() {
                     expect(mockLog.warn).toHaveBeenCalled();
                 });
+            });
+        });
+    });
+
+    describe('getWebsiteData(req, config)', function() {
+        var req, config;
+        var success, failure;
+        var spideyDeferred;
+
+        beforeEach(function(done) {
+            req = {
+                user: { id: 'u-0507ebe9b5dc5d' },
+                body: null,
+                query: {
+                    uri: 'http://www.toyota.com/'
+                },
+                uuid: 'uieyrf7834rg'
+            };
+
+            config = {
+                scraper: {
+                    timeout: 5000,
+                    agent: 'Reelcontent Web Scraper'
+                }
+            };
+
+            success = jasmine.createSpy('success()');
+            failure = jasmine.createSpy('failure()');
+
+            spideyDeferred = q.defer();
+            spidey.and.returnValue(spideyDeferred.promise);
+
+            collateral.getWebsiteData(req, config).then(success, failure);
+            process.nextTick(done);
+        });
+
+        it('should make a request with spidey.js', function() {
+            expect(spidey).toHaveBeenCalledWith(req.query.uri, {
+                timeout: config.scraper.timeout,
+                gzip: true,
+                headers: {
+                    'User-Agent': config.scraper.agent
+                }
+            });
+        });
+
+        describe('when the spidey() call succeeds', function() {
+            var data;
+
+            beforeEach(function(done) {
+                data = {
+                    links: {
+                        website: 'http://www.toyota.com/',
+                        facebook: 'http://www.facebook.com/toyota',
+                        twitter: 'http://twitter.com/toyota',
+                        instagram: 'http://instagram.com/toyotausa/',
+                        youtube: 'http://www.youtube.com/user/ToyotaUSA',
+                        pinterest: null,
+                        google: 'https://plus.google.com/+toyotausa/',
+                        tumblr: null
+                    },
+                    images: {
+                        profile: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/c124.57.712.712/s200x200/399266_10151276650434201_443074649_n.jpg?oh=e6b8cc83da86e05e312beab0daad0d95&oe=56EA86EA&__gda__=1458601243_4b4d11415406f734644c00dd8898c10f'
+                    }
+                };
+
+                spideyDeferred.fulfill(data);
+                process.nextTick(done);
+            });
+
+            it('should fulfill with a [200]', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 200,
+                    body: data
+                }));
+            });
+        });
+
+        describe('if the request times out', function() {
+            var error;
+
+            beforeEach(function(done) {
+                error = new Error('Error: ETIMEDOUT');
+                error.name = 'RequestError';
+                error.cause = new Error('ETIMEDOUT');
+                error.cause.code = 'ETIMEDOUT';
+
+                spideyDeferred.reject(error);
+                process.nextTick(done);
+            });
+
+            it('should fulfill with a [408]', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 408,
+                    body: 'Timed out scraping website [' + req.query.uri + '].'
+                }));
+            });
+
+            it('should log a warning', function() {
+                expect(mockLog.warn).toHaveBeenCalled();
+                expect(mockLog.error).not.toHaveBeenCalled();
+            });
+        });
+
+        describe('if something else goes wrong in request', function() {
+            var error;
+
+            beforeEach(function(done) {
+                error = new Error('Error: BLEH');
+                error.name = 'RequestError';
+                error.cause = new Error('BLEH');
+                error.cause.code = 'EBLEH';
+
+                spideyDeferred.reject(error);
+                process.nextTick(done);
+            });
+
+            it('should fulfill with a 500', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 500,
+                    body: 'Unexpected error fetching website: ' + util.inspect(error)
+                }));
+            });
+
+            it('should log a warning', function() {
+                expect(mockLog.warn).toHaveBeenCalled();
+                expect(mockLog.error).not.toHaveBeenCalled();
+            });
+        });
+
+        describe('if the upstream server responds with a failing status code', function() {
+            var error;
+
+            beforeEach(function(done) {
+                error = new Error('404 - The page could not be found.');
+                error.name = 'StatusCodeError';
+                error.statusCode = 404;
+
+                spideyDeferred.reject(error);
+                process.nextTick(done);
+            });
+
+            it('should fulfill with a [400]', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 400,
+                    body: 'Upstream server responded with status code [404].'
+                }));
+            });
+
+            it('should not log a warning or error', function() {
+                expect(mockLog.warn).not.toHaveBeenCalled();
+                expect(mockLog.error).not.toHaveBeenCalled();
+            });
+        });
+
+        describe('if there is some unknown error', function() {
+            var error;
+
+            beforeEach(function(done) {
+                error = new SyntaxError('You can\'t type.');
+
+                spideyDeferred.reject(error);
+                process.nextTick(done);
+            });
+
+            it('should fulfill with a [500]', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 500,
+                    body: 'Internal error: ' + util.inspect(error)
+                }));
+            });
+
+            it('should log an error', function() {
+                expect(mockLog.warn).not.toHaveBeenCalled();
+                expect(mockLog.error).toHaveBeenCalled();
+            });
+        });
+
+        describe('if the request uri is not valid', function() {
+            beforeEach(function(done) {
+                spidey.and.callThrough();
+                req.query.uri = 'fiurwehrfui4th';
+
+                collateral.getWebsiteData(req, config).then(success, failure).finally(done);
+            });
+
+            it('should succeed with a 400', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 400,
+                    body: 'URI [' + req.query.uri + '] is not valid.'
+                }));
+            });
+
+            it('should not log a warning or error', function() {
+                expect(mockLog.warn).not.toHaveBeenCalled();
+                expect(mockLog.error).not.toHaveBeenCalled();
+            });
+        });
+
+        describe('if a request uri is not specified', function() {
+            beforeEach(function(done) {
+                spidey.and.callThrough();
+                spidey.calls.reset();
+                delete req.query.uri;
+
+                collateral.getWebsiteData(req, config).then(success, failure).finally(done);
+            });
+
+            it('should not attempt to scrape anything', function() {
+                expect(spidey).not.toHaveBeenCalled();
+            });
+
+            it('should succeed with a 400', function() {
+                expect(success).toHaveBeenCalledWith(jasmine.objectContaining({
+                    code: 400,
+                    body: 'Must specify a URI.'
+                }));
+            });
+
+            it('should not log a warning or error', function() {
+                expect(mockLog.warn).not.toHaveBeenCalled();
+                expect(mockLog.error).not.toHaveBeenCalled();
             });
         });
     });
