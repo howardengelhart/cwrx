3fab4396a95ee40b005f4e5299de026465481e87
Joshua Minzner <josh@minzner.org>
2016-02-18 10:27:22 -0500

    Add tracking pixel macros for player/screen dimensions.

--- test/unit/adLoader.spec.js
+++ test/unit/adLoader.spec.js
@@ -1304,7 +1304,7 @@ describe('AdLoader()', function() {
                                 ex: meta.ex,
                                 vr: meta.vr,
                                 event: 'play'
-                            }) + '&c={context}&d={delay}&cb={cachebreaker}');
+                            }) + '&sw={screenWidth}&sh={screenHeight}&pw={playerWidth}&ph={playerHeight}&c={context}&d={delay}&cb={cachebreaker}');
                         });
 
                         describe('if there is no trackingPixel', function() {
@@ -1342,7 +1342,7 @@ describe('AdLoader()', function() {
                                     ex: meta.ex,
                                     vr: meta.vr,
                                     event: 'q1'
-                                }) + '&c={context}&d={delay}&cb={cachebreaker}');
+                                }) + '&sw={screenWidth}&sh={screenHeight}&pw={playerWidth}&ph={playerHeight}&c={context}&d={delay}&cb={cachebreaker}');
                             });
                         });
                     });
