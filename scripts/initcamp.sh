d2d3c64753ed813c392c48547e62d5562c517be6
Howard Engelhart <howard.engelhart@outlook.com>
2016-03-10 11:13:50 -0500

    Changed querybot to use campaign_summary_hourly table directly rather than the campaign_summary_hourly_all view.

--- scripts/initcamp.sh
+++ scripts/initcamp.sh
@@ -51,17 +51,17 @@ export PGDATABASE='campfire_cwrx'
 psql -c "CREATE SCHEMA rpt;"
 psql -c "GRANT USAGE ON SCHEMA rpt TO viewer;"
 
-read -r -d '' campaign_summary_hourly_all <<- EOM
-CREATE TABLE rpt.campaign_summary_hourly_all
+read -r -d '' campaign_summary_hourly <<- EOM
+CREATE TABLE rpt.campaign_summary_hourly
 (
     rec_ts timestamp with time zone,
     campaign_id character varying(20),
     event_type character varying(100),
     events bigint,
     event_cost numeric(12,4),
-    CONSTRAINT uc_campaign_summary_hourly_all UNIQUE (rec_ts,campaign_id,event_type)
+    CONSTRAINT uc_campaign_summary_hourly UNIQUE (rec_ts,campaign_id,event_type)
 ) WITH ( OIDS=FALSE);
 EOM
 
-psql -c "${campaign_summary_hourly_all}"
-psql -c "GRANT SELECT ON TABLE rpt.campaign_summary_hourly_all TO viewer;"
+psql -c "${campaign_summary_hourly}"
+psql -c "GRANT SELECT ON TABLE rpt.campaign_summary_hourly TO viewer;"
