79e04f914174886cdfaa7d1bb21174b0d2e02def
Evan Leichter <evan.leichter@gmail.com>
2016-03-21 16:06:44 -0400

    cleanup + add changelog

--- tasks/vagrant.js
+++ tasks/vagrant.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
         spawn   = require('child_process').spawn;
 
     var serviceDepends = {
+        accountant  : 'auth,monitor,ads,orgSvc',
         ads         : 'auth,content,monitor,orgSvc,geo',
         collateral  : 'auth,monitor',
         geo         : 'auth,monitor',
