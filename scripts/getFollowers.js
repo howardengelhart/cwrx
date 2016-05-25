019381d049fcb8e144143c159063bca1d86a5fc5
Olivia Shanley <olivia.shanley@gmail.com>
2016-05-25 14:34:43 -0400

    Updated script to allow for both ID-only requests and username requests

--- scripts/getFollowers.js
+++ scripts/getFollowers.js
@@ -15,16 +15,35 @@ var numFollowers = 0;
 var userData = [];
 var noDups = [];
 var cursor = -1;
-
-
+var idsOnly;
+var prefVar;
+var count = 5000;
 //Check and Process Username Argument from Command Line
   if (process.argv[2] == null) {
-    console.log("Error: Please provide username argument");
+    console.log("Error: Please provide arguments for username and the boolean value IDsOnly");
     process.exit();
   }
   else {
     userName = process.argv[2];
   }
+
+  //Check and Process "IDs Only" Argument from Command Line
+    if (process.argv[3] == "true"){
+      idsOnly = true;
+      prefVar = "ids";
+    }
+    else if (process.argv[3] == "false"){
+      idsOnly = false;
+      prefVar = "list";
+
+    }
+    else if ((process.argv[2] == "true") || (process.argv[2] == "false")){
+      console.log("Error: Please provide username argument");
+    }
+    else{
+      console.log("Error: Please provide boolean argument for 'IDs Only'");
+      process.exit();
+    }
 
 //Request Bearer Token
 var options1 = {
@@ -57,12 +76,13 @@ rp(options1)
         function getFollowers(cursor, authToken)  {
           //Request Followers
           var options2 =  {
-            uri: 'https://api.twitter.com/1.1/followers/list.json',
+            uri: 'https://api.twitter.com/1.1/followers/' + prefVar + '.json',
             qs: {
               screen_name: userName,
               include_user_entities: false,
               skip_status: true,
-              cursor : cursor
+              cursor : cursor,
+              count: count
             },
             headers:  {
               'User-Agent': 'Reelcontent',
@@ -74,14 +94,21 @@ rp(options1)
             return rp(options2)
 
             .then(function(twitterResponse) {
-              twitterResponse.users.forEach(function(entry)
-              {
-                var user_id = entry.id + "";
-                var screen_name = entry.screen_name + "";
-                var name = entry.name + "";
-                userData.push(screen_name + "," + user_id + "," + name);
-              });
 
+              if (idsOnly == false){
+                twitterResponse.users.forEach(function(user)
+                {
+                  var user_id = user.id + "";
+                  var screen_name = user.screen_name + "";
+                  var name = user.name + "";
+                  userData.push(screen_name + "," + user_id + "," + name);
+                });
+              }
+              else{
+                twitterResponse.ids.forEach(function(user) {
+                  userData.push(user);
+                });
+              }
               //Change Cursor Value to next_cursor to Print Out Next Page of Results
               cursor = twitterResponse.next_cursor;
 
